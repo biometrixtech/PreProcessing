45fa2834e657a50fc25e7fe94288c084909b6470
Ankur Manikandan <ankur@biometrixtech.com>
2016-06-17 16:33:13 -0400

    made changes to the header

--- app/src/Version_1.1/load_calc.py
+++ app/src/Version_1.1/load_calc.py
@@ -29,6 +29,9 @@ def calc_force(ma, ema, a):
 Inputs: Acceleration and phase exploration data (AccX, AccY, AccZ, Phase) of the right heel, left heel and hip 
         sensors; the mass of the user in kilograms; any extra mass the user has strapped on in kilograms
 Outputs: array of the load during balance phase, on the right foot (rf_bal) and on the left foot (lf_bal) 
+Datasets: PreProcessing\data\load_calc; 3 input files (lbal_hip_in.csv, lbal_lfoot_in.csv, lbal_rfoot_in.csv
+          -> load_balance(rdata, ldata, hipdata, user_mass, user_extramass)
+          -> 2 output files (lbal_lfoot_out.csv, lbal_rfoot_out.csv)
 #############################################################################################################
 """
 
@@ -84,10 +87,10 @@ if __name__ == '__main__':
     exmass = 0 #in kilograms
     ld_rf, ld_lf = load_balance(rdata, ldata, hdata, mass, exmass) #passing the right foot, left foot, hip, user mass and extra mass data
     
-    plt.plot(ld_rf)
-    plt.plot(ld_lf)
+    #plt.plot(ld_rf)
+    #plt.plot(ld_lf)
     #plt.plot(ldata['AccZ'])
     #plt.plot(ph)
-    plt.show()
+    #plt.show()
     
     
