c5cca92de945468c743fa2dccf0ce2ac2f2d3cc3
brian-biometrix <brian@biometrixtech.com>
2016-06-29 12:59:30 -0400

    addeddictionary of list CME thresholds and passes through to CME detection algo.

--- app/src/Version_1.2/CME_Detect.py
+++ app/src/Version_1.2/CME_Detect.py
@@ -10,25 +10,48 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import peak_det as peak
 import phase_exploration as phase
+import Data_Processing as prep
 
 """
 #############################################INPUT/OUTPUT####################################################   
-Inputs: peak detection for each sensor on each axis, body phase detection, list of states for evaluation
+Inputs: continuous orientation data, body phase detection, list of states for evaluation, neutral position for
+axis, list of thresholds
 
-Outputs: 3 lists with [angular displacement change], [upper time interval], [lower time interval] for each CME
-and each foot
+Outputs: array [time stamp, normalization score, indic of positive or negative, angle difference from neutral]
 
 Datasets: phase_inputs.csv, peak_inputs.csv (contains both peak and trough values) ->
 rot_CME(maxtab, mintab, output, [0,1]) -> outputs.csv
 #############################################################################################################
 """
-def rot_CME(maxtab, mintab, states, state):
-    combine = np.concatenate((maxtab, mintab)) #combine min and max array
-    combine = combine[combine[:,0].argsort()] #sort by index to get in chronological order
-    mags = [[(180/np.pi)*(combine[i,1]-combine[i-1,1]), combine[i,0], combine[i-1,0]] for i in range(1,len(combine))] #calculate magnitude of change and pass time stamps    
-    final = [mags[i] for i in range(1,len(mags)) if states[int(mags[i][1])] in state] #filter out by state
-    final = np.array(final)
-    return final[:,0], final[:,1], final[:,2]
+def cont_norm_score(mag, quat, cme):
+    la = cme[0]
+    le = cme[1]
+    ua = cme[2]
+    ue = cme[3]
+    dev = quat-mag
+    if 0 < dev <= ua:
+        return 1, np.sign(dev), dev
+    elif ua < dev < ue:
+        return 1 - ((dev-ua)/(ue-ua)), np.sign(dev), dev
+    elif dev >= ue:
+        return 0, np.sign(dev), dev
+    elif la <= dev <= 0:
+        return 1, np.sign(dev), dev
+    elif le < dev < la:
+        return 1 - ((dev-la)/(le-la)), np.sign(dev), dev
+    elif dev <= le:
+        return 0, np.sign(dev), dev
+    
+    
+def cont_rot_CME(series, states, state, rel, cme):
+    out = []
+    for i in range(len(series)):
+        if states[i] in state:
+            ang = (180/np.pi)*series[i]
+            comp = (180/np.pi)*rel
+            n, indic, raw = cont_norm_score(ang, comp, cme)
+            out.append([i, n, indic, raw])
+    return np.array(out)
     
 if __name__ == "__main__":
     pos = 'lf'
@@ -40,9 +63,14 @@ if __name__ == "__main__":
 #    rroot = 'C:\\Users\\Brian\\Documents\\Biometrix\\Data\\Collected Data\\BodyFrame walking\\Rheel_Gabby_walking_heeltoe_set1.csv'
 #    hroot = 'C:\\Users\\Brian\\Documents\\Biometrix\\Data\\Collected Data\\BodyFrame walking\\hips_Gabby_walking_heeltoe_set1.csv'    
     
+    cme_dict = {'prosupl':[-4, -7, 4, 15], 'hiprotl':[-4, -7, 4, 15], 'hipdropl':[-4, -7, 4, 15],
+                'prosupr':[-4, -15, 4, 7], 'hiprotr':[-4, -15, 4, 7], 'hipdropr':[-4, -15, 4, 7]}    
+    
     rdata = pd.read_csv(rroot)
     ldata = pd.read_csv(lroot)
     hdata = pd.read_csv(hroot)
+    
+    neutral = np.matrix([1,0,0,0]) # stand-in, will come from anatomical fix module    
     
     ##Phase Detection
     rseries = rdata['AccZ'].values
@@ -52,15 +80,13 @@ if __name__ == "__main__":
     output = phase.Body_Phase(rseries, lseries, rpitch, lpitch, 250)
     ldata['Phase'] = output
     rdata['Phase'] = output
-    hdata['Phase'] = output   
+    hdata['Phase'] = output 
     
-    #Peak Detection- Right Foot Pronation
-    peak_series = hdata['EulerZ'].values
-    maxtab, mintab = peak.peakdet(peak_series, .05)
+    peak_series = ldata['EulerX'].values
+    neutral_eul = prep.Calc_Euler(neutral)
     
     ###THE GOOD STUFF!!!####
-    if len(maxtab) != 0 or len(mintab) != 0:
-        rot_mag, upper_lim, lower_lim = rot_CME(maxtab, mintab, output, [1,0])
+    out = cont_rot_CME(peak_series, output, [0,1], neutral_eul[0], cme_dict['prosupl'])
 
     
     
