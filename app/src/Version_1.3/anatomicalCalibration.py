78bbfba38a523937b20b1e1ea6ebca42011c2351
brian-biometrix <brian@biometrixtech.com>
2016-07-08 12:26:47 -0400

    Adding v1.3 folder.

---
+++ app/src/Version_1.3/anatomicalCalibration.py
@@ -0,0 +1,267 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 28 16:51:34 2016
+
+@author: Brian
+"""
+import pandas as pd
+import numpy as np
+import coordinateFrameTransformation as prep
+
+class MovementError(Exception):
+    pass
+
+class DataCollectionError(Exception):
+    pass
+
+class BowSpeedError(Exception):
+    pass
+
+def direct(x,y):
+    #choose correct direction for prediction vector
+    if abs(x) > abs(y):
+        return np.sign(x)
+    if abs(y) > abs(x):
+        return -np.sign(y)
+
+def eul2dcm(eul):
+    #set euler angles
+    x = eul[0]
+    y = eul[1]
+    z = eul[2]
+    
+    #calc euler angles with sin and cosin
+    cx = np.cos(x)
+    sx = np.sin(x)
+    cy = np.cos(y)
+    sy = np.sin(y)
+    cz = np.cos(z)
+    sz = np.sin(z)
+    
+    #make into rotation matrices
+    rx = np.matrix([[1,0,0],[0,cx,-sx],[0,sx,cx]])
+    ry = np.matrix([[cy,0,sy],[0,1,0],[-sy,0,cy]])
+    rz = np.matrix([[cz,-sz,0],[sz,cz,0],[0,0,1]])
+    
+    #multiply rotation matrices together to get complete rotation matrix
+    R = rz.dot(ry.dot(rx))
+    return R
+
+def dcm2q(dcm):
+    T = dcm[0,0] + dcm[1,1] + dcm[2,2]
+    if T > 10**-8:
+        qw = 0.5*np.sqrt(1+T)
+        qx = (dcm[2,1]-dcm[1,2])/(4*qw)
+        qy = (dcm[0,2]-dcm[2,0])/(4*qw)
+        qz = (dcm[1,0]-dcm[0,1])/(4*qw)
+    elif (dcm[0,0] > dcm[1,1]) & (dcm[0,0] > dcm[2,2]):
+        S = np.sqrt(1 + dcm[0,0] - dcm[1,1] - dcm[2,2])*2
+        qw = (dcm[2,1]-dcm[1,2])/S
+        qx = 0.25*S
+        qy = (dcm[0,1]+dcm[1,0])/S
+        qz = (dcm[0,2]+dcm[2,0])/S
+    elif (dcm[1,1]>dcm[2,2]):
+        S = np.sqrt(1 + dcm[1,1] - dcm[0,0] - dcm[2,2])*2
+        qw = (dcm[0,2]-dcm[2,0])/S
+        qx = (dcm[0,1]+dcm[1,0])/S
+        qy = 0.25*S
+        qz = (dcm[1,2]+dcm[2,1])/S
+    else:
+        S = np.sqrt(1 + dcm[2,2] - dcm[0,0] - dcm[1,1])*2
+        qw = (dcm[1,0]-dcm[0,1])/S
+        qx = (dcm[0,2]+dcm[2,0])/S
+        qy = (dcm[1,2]+dcm[2,1])/S
+        qz = 0.25*S   
+    q = np.matrix([qw, qx, qy, qz])
+    return q
+    
+def hip_orientation_fix(hips, ref, hz):
+    gyr_x = hips['gyrX'].values
+    gyr_y = hips['gyrY'].values
+    eul = hips['EulerZ'].values
+    
+    expect = []
+    length = []
+    for i in range(len(gyr_x)):
+        actual = np.array([gyr_x[i], gyr_y[i]]) #create actual vector
+        posneg = direct(gyr_x[i], gyr_y[i]) #choose direction of prediction vector
+        mag = posneg*np.sqrt(actual.dot(actual.transpose())) #find magnitude of prediction vector (with direction)
+        length.append(mag) #append mag
+        diff = ref - eul[i] #find yaw rotation away from neutral reference
+        R = np.array([[np.cos(diff), -np.sin(diff)],[np.sin(diff), np.cos(diff)]]) #create rotation matrix
+        naive = np.array([mag,0]) #create naive prediction vector
+        expect.append(R.dot(naive)) #create actual prediction vector and append to list of pred vectors
+    
+    theta = []
+    ang_indic = []
+    for i in range(len(gyr_x)):
+        if abs(length[i]) > 10: #only care about vectors greater than 10 magnitude
+            exp = expect[i] #get predicted vector
+            actual = np.array([gyr_x[i], gyr_y[i]]) #get actual vector
+            sign = np.sign(exp[1]*exp[0]) #determine sign of theta
+            angle = np.arccos((exp.dot(actual))/(np.linalg.norm(exp)*np.linalg.norm(actual))) #find mag of theta
+            theta.append(sign*angle) #combine to get theta for data point
+        #keep track of which points are classified as moving and which weren't
+        if abs(length[i]) > 10:
+            ang_indic.append(20)
+        else:
+            ang_indic.append(0)
+    #throw error if bow happened too fast
+    if ang_indic.count(20) < hz*1.5:
+        print('Bow too fast, please try again')
+        raise BowSpeedError
+    #throw error if bow seems to have been cut off at end
+    if ang_indic[-int(hz*.1):].count(20) > 1:
+        print('Bow too slow, please try again')
+        raise BowSpeedError
+    #take avg of theta list to get final theta vector       
+    fin_theta = np.mean(theta)
+    
+    #create hip fix quaternion
+    w = np.cos(fin_theta/2)
+    if fin_theta < 0:
+        z = -np.sqrt(1-w**2) #compute 4th term of offset quaternion
+    else:
+        z = np.sqrt(1-w**2)
+    yaw_fix = np.matrix([w, 0, 0, z])
+    return yaw_fix
+
+def init_orientation(data, err_tol):
+    roll_val = []
+    ptch_val = []
+    yaw_val = []
+    indic = []
+    for i in range(len(data)):
+        #consider points that can be classified as not moving
+        if np.sqrt(data.ix[i,'gyrX_raw']**2 + data.ix[i,'gyrY_raw']**2 + data.ix[i,'gyrZ_raw']**2) < 10:
+            q = np.matrix([data.ix[i,'qW_raw'], data.ix[i,'qX_raw'], data.ix[i,'qY_raw'], data.ix[i,'qZ_raw']]) #get quaternion 
+            eul = prep.Calc_Euler(q) #calculate euler angle
+            #append euler angles to respective lists
+            roll_val.append(eul[0])
+            ptch_val.append(eul[1])
+            yaw_val.append(eul[2])
+        else:
+            #keep track of how many points involved movement
+            indic.append(0)
+    #if movement time exceeds expected amount ask to stop moving
+    if len(indic) > err_tol*len(data):
+        print('Do not move during calibration')
+        raise MovementError
+    else:
+        #create list of euler angle averages
+        eul_avg = [np.mean(roll_val),np.mean(ptch_val),np.mean(yaw_val)]
+        R = eul2dcm(eul_avg) #create rotation matrix from euler angles
+        q = dcm2q(R) #create quaternion from rotation matrix
+        return q
+
+def orient_feet(foot, hip):
+    #calc euler angles of neutral quaternions
+    eul_ft = prep.Calc_Euler(foot)
+    eul_hp = prep.Calc_Euler(hip)
+    #find diff between foot and hip heading
+    diff = eul_ft[2]-eul_hp[2]
+    
+    #convert diff in heading to quaternion for each foot
+    w = np.cos(diff/2)
+    if diff < 0:
+        z = -np.sqrt(1-w**2) #compute 4th term of offset quaternion
+    else:
+        z = np.sqrt(1-w**2)
+    yaw_fix = np.matrix([w, 0, 0, z])
+    return yaw_fix
+
+def pitch_offset(q):
+    f_dcm = prep.q2dcm(q) #call q2dcm to convert quaternion to rotation matrix
+    ptch = np.arcsin(-f_dcm[2,0]) #calculate pitch of transformation using rotation matrix
+    q0 = np.cos(ptch/2) #compute first term of offset quaternion
+    if ptch < 0:
+        q2 = -np.sqrt(1-q0**2) #compute 3rd term of offset quaternion
+    else:
+        q2 = np.sqrt(1-q0**2)
+    yaw_fix = np.matrix([q0, 0, q2, 0]) #build pitch offset quaternion
+    return yaw_fix
+
+def test_errors(hdata, rdata, ldata, hz):
+    thresh = .3
+    #calculate euler angles of t=0 data point
+    hq0 = np.matrix([hdata.ix[0,'qW_raw'], hdata.ix[0,'qX_raw'], hdata.ix[0,'qY_raw'], hdata.ix[0,'qZ_raw']])
+    rq0 = np.matrix([rdata.ix[0,'qW_raw'], rdata.ix[0,'qX_raw'], rdata.ix[0,'qY_raw'], rdata.ix[0,'qZ_raw']])
+    lq0 = np.matrix([ldata.ix[0,'qW_raw'], ldata.ix[0,'qX_raw'], ldata.ix[0,'qY_raw'], ldata.ix[0,'qZ_raw']])
+    thetah = prep.Calc_Euler(hq0)
+    thetar = prep.Calc_Euler(rq0)
+    thetal = prep.Calc_Euler(lq0)
+    
+    #test for quaternion convergence
+    for i in range(1,hz):
+        #calc euler angles for each sensor at time=i
+        hq = np.matrix([hdata.ix[i,'qW_raw'], hdata.ix[i,'qX_raw'], hdata.ix[i,'qY_raw'], hdata.ix[i,'qZ_raw']])
+        rq = np.matrix([rdata.ix[i,'qW_raw'], rdata.ix[i,'qX_raw'], rdata.ix[i,'qY_raw'], rdata.ix[i,'qZ_raw']])
+        lq = np.matrix([ldata.ix[i,'qW_raw'], ldata.ix[i,'qX_raw'], ldata.ix[i,'qY_raw'], ldata.ix[i,'qZ_raw']])
+        hang = prep.Calc_Euler(hq)
+        rang = prep.Calc_Euler(rq)
+        lang = prep.Calc_Euler(lq)
+        #if diff between t=0 and t=i euler angle is greater than .2 declare a convergence issue
+        if abs(hang[0] - thetah[0]) > thresh or abs(hang[1]-thetah[1]) > thresh or abs(hang[2]-thetah[2]) > thresh:
+            print("hip", i)
+            print('Technical error, please try again')
+            raise DataCollectionError
+        elif abs(rang[0] - thetar[0]) > thresh or abs(rang[1]-thetar[1]) > thresh or abs(rang[2]-thetar[2]) > thresh:
+            print("right", i)
+            print('Technical error, please try again')
+            raise DataCollectionError
+        elif abs(lang[0] - thetal[0]) > thresh or abs(lang[1]-thetal[1]) > thresh or abs(lang[2]-thetal[2]) > thresh:
+            print("left", i)
+            print('Technical error, please try again')
+            raise DataCollectionError
+        else:
+            pass
+
+if __name__ == "__main__":    
+    hroot = 'C:\\Users\\Brian\\Documents\\Biometrix\\Data\\Collected Data\\Alignment test\\bow13.csv'
+    rroot = 'C:\\Users\\Brian\\Documents\\Biometrix\\Data\\Collected Data\\Alignment test\\rbow13.csv'
+    lroot = 'C:\\Users\\Brian\\Documents\\Biometrix\\Data\\Collected Data\\Alignment test\\lbow13.csv'
+    
+    hz = 100
+    hdata = pd.read_csv(hroot)
+    rdata = pd.read_csv(rroot)
+    ldata = pd.read_csv(lroot)
+    #create vector that represents 90 degree heading rotation
+    xy_switch = np.matrix([1,0,0,-1])/np.linalg.norm([1,0,0,-1]) 
+    
+    test_errors(hdata, rdata, ldata, hz) #test for convergence of quaternions    
+    
+    #find anatomically neutral orientation for each sensor and decide if any movement errors    
+    h_q = init_orientation(hdata, .60)
+    r_q = init_orientation(rdata, .03)
+    l_q = init_orientation(ldata, .03)
+    
+    bodyframe = []
+    for i in range(len(hdata)):
+        gyro = np.matrix([0, hdata.ix[i,'gyrX_raw'], hdata.ix[i,'gyrY_raw'], hdata.ix[i,'gyrZ_raw']]) #get gyr quaternion
+        q = np.matrix([hdata.ix[i,'qW_raw'], hdata.ix[i,'qX_raw'], hdata.ix[i,'qY_raw'], hdata.ix[i,'qZ_raw']]) #get orientation quaternion
+        yaw_fix = prep.yaw_offset(q) #uses yaw offset function above to compute yaw offset quaternion
+        yfix_c = prep.QuatConj(yaw_fix) #offset the yaw at every point
+        qf = prep.QuatProd(yfix_c, q) #create new orient quat with yaw offset
+        yaw = prep.Calc_Euler(q)[2] #calculate yaw of original orientation quaternion to track heading
+        bodyframe.append(np.append(prep.rotate_quatdata(gyro,qf),yaw))
+    
+    hglob = pd.DataFrame(bodyframe, columns=["gyrX", "gyrY", "gyrZ", "EulerZ"]) #create dataframe of corrected gyro, and heading data
+    hfx_q = hip_orientation_fix(hglob, prep.Calc_Euler(h_q)[2], hz) #find degree offset of hip sensor from forward
+    fixed_h = prep.QuatProd(prep.QuatConj(xy_switch), prep.QuatProd(prep.QuatConj(hfx_q), h_q)) #correct heading of hip sensor
+    #find pitch offset of anatomically neutral position of each sensor
+    pitch_alignl_q = pitch_offset(l_q)
+    pitch_alignr_q = pitch_offset(r_q)
+    pitch_alignh_q = pitch_offset(h_q)
+    #find feet offset to true forward
+    yaw_alignl_q = orient_feet(l_q, fixed_h)
+    yaw_alignr_q = orient_feet(r_q, fixed_h)
+    #combine true forward offset and pitch offset 
+    alignl_q = prep.QuatProd(pitch_alignl_q, yaw_alignl_q)
+    alignr_q = prep.QuatProd(pitch_alignr_q, yaw_alignr_q)
+    alignh_q = prep.QuatProd(prep.QuatConj(pitch_alignh_q), prep.QuatConj(hfx_q))
+
+    neutral_lq = prep.QuatProd(yaw_alignl_q, prep.QuatProd(prep.QuatConj(pitch_alignl_q),prep.QuatProd(prep.QuatConj(prep.yaw_offset(l_q)), l_q)))
+    neutral_rq = prep.QuatProd(yaw_alignr_q, prep.QuatProd(prep.QuatConj(pitch_alignr_q),prep.QuatProd(prep.QuatConj(prep.yaw_offset(r_q)), r_q)))
+    neutral_hq = prep.QuatProd(hfx_q, prep.QuatProd(prep.QuatConj(pitch_alignh_q),prep.QuatProd(prep.QuatConj(prep.yaw_offset(h_q)), h_q)))
+    print('Success!')
+        
---
+++ app/src/Version_1.3/executionScore.py
@@ -0,0 +1,273 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 24 09:47:48 2016
+@author: Ankur
+"""
+
+import numpy as np
+
+"""
+#############################################INPUT/OUTPUT####################################################
+Function: weight_load
+Inputs: normalized scores for, contralateral hip drop (right and left feet); pronation/supination (right and
+        left feet); lateral hip rotation (right and left feet, both single and double); landing time; landing
+        pattern; load array (output of the load_bal_imp function)
+Outputs: THE EXECUTION SCORE!
+#############################################################################################################
+"""
+
+def weight_load(nrf_contra, nlf_contra, nrf_prosup, nlf_prosup, nrf_hiprot, nlf_hiprot, nrfdbl_hiprot, nlfdbl_hiprot, nlndtme, nlndptn, load):
+    
+    #ASSIGNING THE POINTS TO EACH CME
+    #Contralateral Hip Drop
+    lambda_contra = 10.0
+    #Pronation/Supination
+    lambda_prosup = 15.0
+    #Lateral Hip Drop
+    lambda_hiprot = 5.0
+    #Landing Time
+    lambda_landtime = 2.5
+    #Landing Pattern
+    lambda_landpattern = 2.5
+  
+    #DETERMINING THE LOAD ON THE RIGHT AND LEFT FEET FOR EACH CME    
+    #Contralateral Hip Drop   
+    pos = loadrf_sum = loadlf_sum = br = bl = 0
+    #Right foot
+    for i in range(len(nrf_contra)):
+        a = b = 0
+        pos = int(nrf_contra[i,0])
+        if load[pos,2] == 0 or load[pos,2] == 2:
+            a = nrf_contra[i,1] * load[pos,0] * lambda_contra
+            b = load[pos,0]
+            br = br + a
+            loadrf_sum = loadrf_sum + b
+    #Left foot
+    for i in range(len(nlf_contra)):
+        a = b = 0
+        pos = int(nlf_contra[i,0])
+        if load[pos,3] == 0 or load[pos,3] == 1:
+            a = nlf_contra[i,1]*load[pos,1] * lambda_contra
+            b = load[pos,1]
+            bl = bl + a
+            loadlf_sum = loadlf_sum + b
+    #P_contra       
+    pcontra = (br/(loadrf_sum + loadlf_sum)) + (bl/(loadrf_sum + loadlf_sum))
+    
+    #Pronation/Supination    
+    pos = loadrf_sum = loadlf_sum = br = bl = 0
+    #Right foot
+    for i in range(len(nrf_prosup)):
+        a = b = 0
+        pos = int(nrf_prosup[i,0])
+        if load[pos,2] == 0 or load[pos,2] == 2:
+            a = nrf_prosup[i,1] * load[pos,0] * lambda_prosup
+            b = load[pos,0]
+            br = br + a
+            loadrf_sum = loadrf_sum + b
+    #Left foot
+    for i in range(len(nlf_prosup)):
+        a = b = 0
+        pos = int(nlf_prosup[i,0])
+        if load[pos,3] == 0 or load[pos,3] == 1:
+            a = nlf_prosup[i,1]*load[pos,1] * lambda_prosup
+            b = load[pos,1]
+            bl = bl + a
+            loadlf_sum = loadlf_sum + b
+    #P_pro/sup       
+    pprosup = (br/(loadrf_sum + loadlf_sum)) + (bl/(loadrf_sum + loadlf_sum))
+    
+    #Lateral Hip Rotation
+    pos = loadrf_sum = loadlf_sum = br = bl = loadrfdbl_sum = loadlfdbl_sum = brdbl = bldbl = 0
+    #Right foot (Single and double)
+    for i in range(len(nrf_hiprot)):
+        a = b = 0
+        pos = int(nrf_hiprot[i,0])
+        if load[pos,2] == 2:
+            a = nrf_hiprot[i,1] * load[pos,0] * lambda_hiprot
+            b = load[pos,0]
+            br = br + a
+            loadrf_sum = loadrf_sum + b
+    for i in range(len(nrfdbl_hiprot)):
+        a = b = 0
+        pos = int(nrfdbl_hiprot[i,0])
+        if load[pos,2] == 0:
+            a = nrfdbl_hiprot[i,1] * load[pos,0] * lambda_hiprot
+            b = load[pos,0]
+            brdbl = brdbl + a
+            loadrfdbl_sum = loadrfdbl_sum + b
+    #Left foot (Single and Double)
+    for i in range(len(nlf_hiprot)):
+        a = b = 0
+        pos = int(nlf_hiprot[i,0])
+        if load[pos,3] == 1:
+            a = nlf_hiprot[i,1]*load[pos,1] * lambda_hiprot
+            b = load[pos,1]
+            bl = bl + a
+            loadlf_sum = loadlf_sum + b
+    for i in range(len(nlfdbl_hiprot)):
+        a = b = 0
+        pos = int(nlfdbl_hiprot[i,0])
+        if load[pos,3] == 0:
+            a = nlfdbl_hiprot[i,1]*load[pos,1] * lambda_hiprot
+            b = load[pos,1]
+            bldbl = bldbl + a
+            loadlfdbl_sum = loadlfdbl_sum + b
+    #P_hiprot
+    phiprot = (br/(loadrf_sum + loadlf_sum + loadrfdbl_sum + loadlfdbl_sum)) + (bl/(loadrf_sum + loadlf_sum  + loadrfdbl_sum + loadlfdbl_sum)) + (brdbl/(loadrf_sum + loadlf_sum + loadrfdbl_sum + loadlfdbl_sum)) + (bldbl/(loadrf_sum + loadlf_sum  + loadrfdbl_sum + loadlfdbl_sum))
+        
+    #Impact CMEs
+    if len(nlndtme) != 0:
+        #Landing Time (Double leg only)
+        pos = loadrfdbl_sum = loadlfdbl_sum = brdbl = bldbl = total_loadrfdbl_sum = total_loadlfdbl_sum = 0
+        #Right foot
+        for i in range(len(nlndtme)):
+            pos = int(nlndtme[i,0])
+            for j,k in zip(load[pos:,0], load[pos:,2]):
+                if k == 4:
+                    loadrfdbl_sum = loadrfdbl_sum + j
+                elif k != 4:
+                    break
+            brdbl = loadrfdbl_sum * nlndtme[i,2] * lambda_landtime + brdbl
+            total_loadrfdbl_sum = loadrfdbl_sum + total_loadrfdbl_sum
+        #Left foot
+        for i in range(len(nlndtme)):
+            pos = int(nlndtme[i,1])
+            for j,k in zip(load[pos:,1], load[pos:,3]):
+                if k == 4:
+                    loadlfdbl_sum = loadlfdbl_sum + j
+                elif k != 4:
+                    break
+            bldbl = loadlfdbl_sum * nlndtme[i,3] * lambda_landtime + bldbl
+            total_loadlfdbl_sum = total_loadlfdbl_sum + loadlfdbl_sum
+        #P_landtime
+        plandtime = (brdbl/(total_loadrfdbl_sum + total_loadlfdbl_sum)) + (bldbl/(total_loadrfdbl_sum + total_loadlfdbl_sum))
+    
+        #Landing Pattern (Double only)
+        pos = loadrfdbl_sum = loadlfdbl_sum = brdbl = bldbl = total_loadrfdbl_sum = total_loadlfdbl_sum = 0
+        #Right foot
+        for i in range(len(nlndptn)):
+            pos = int(nlndptn[i,0])
+            for j,k in zip(load[pos:,0], load[pos:,2]):
+                if k == 4:
+                    loadrfdbl_sum = loadrfdbl_sum + j
+                elif k != 4:
+                    break
+            brdbl = loadrfdbl_sum * nlndptn[i,2] + brdbl
+            total_loadrfdbl_sum = loadrfdbl_sum + total_loadrfdbl_sum
+        #Left foot
+        for i in range(len(nlndptn)):
+            pos = int(nlndptn[i,1])
+            for j,k in zip(load[pos:,1], load[pos:,3]):
+                if k == 4:
+                    loadlfdbl_sum = loadlfdbl_sum + j
+                elif k != 4:
+                    break
+            bldbl = loadlfdbl_sum * nlndptn[i,3] + bldbl
+            total_loadlfdbl_sum = total_loadlfdbl_sum + loadlfdbl_sum
+        #P_landpattern
+        plandpattern = (brdbl/(total_loadrfdbl_sum + total_loadlfdbl_sum)) + (bldbl/(total_loadrfdbl_sum + total_loadlfdbl_sum))
+    else:
+        plandtime = plandpattern = 0
+        
+    #DETERMINING THE EXECUTION SCORE
+    s = (pcontra + pprosup + phiprot + plandtime + plandpattern)/(lambda_contra + lambda_prosup + lambda_hiprot + lambda_landtime + lambda_landpattern)    
+    
+    #print pcontra, pprosup, phiprot, plandtime, plandpattern
+    
+    return s*100
+    
+if __name__ == "__main__":
+    
+    import pandas as pd
+    from impact_phase import impact_phase
+    from phase_exploration import *
+    import CME_Detect as cmed
+    import Data_Processing as prep
+    from load_calc import load_bal_imp
+    from impact_cme import *
+    
+    rpath = 'C:\\Users\\Ankur\\python\\Biometrix\\Data analysis\\data exploration\\data files\\Subject5\\Subject5_rfdatabody_LESS.csv'
+    #rpath = 'C:\Users\Ankur\python\Biometrix\Data analysis\data exploration\data files\Subject5\Subject5_rfdatabody_snglsquat_set1.csv'
+    lpath = 'C:\\Users\\Ankur\\python\\Biometrix\\Data analysis\\data exploration\\data files\\Subject5\\Subject5_lfdatabody_LESS.csv'
+    #lpath = 'C:\Users\Ankur\python\Biometrix\Data analysis\data exploration\data files\Subject5\Subject5_lfdatabody_snglsquat_set1.csv'
+    #lpath = 'C:\Users\Ankur\python\Biometrix\Data analysis\data exploration\data files\Stomp\Lheel_Gabby_stomp_set1.csv'
+    #lpath = 'C:\Users\Ankur\python\Biometrix\Data analysis\data exploration\data files\ChangeDirection\Lheel_Gabby_changedirection_set1.csv'
+    #lpath = 'C:\Users\Ankur\python\Biometrix\Data analysis\data exploration\data files\Jump\Lheel_Gabby_jumping_explosive_set2.csv'
+    #lpath = 'C:\Users\Ankur\python\Biometrix\Data analysis\data exploration\data files\Walking\Lheel_Gabby_walking_heeltoe_set1.csv'
+    hpath = 'C:\\Users\\Ankur\\python\\Biometrix\\Data analysis\\data exploration\\data files\\Subject5\\Subject5_hipdatabody_LESS.csv'
+    #hpath = 'C:\Users\Ankur\python\Biometrix\Data analysis\data exploration\data files\Subject5\Subject5_hipdatabody_snglsquat_set1.csv'
+
+    rdata = pd.read_csv(rpath)
+    ldata = pd.read_csv(lpath)
+    hdata = pd.read_csv(hpath)
+    
+    #print len(rdata), len(ldata)
+    
+    #acc = ['AccX', 'AccY', 'AccZ']
+    #rdata = rdata[acc]
+    #ldata = ldata[acc]
+    #hdata = hdata[acc]
+    
+    sampl_rate = 250
+    
+    comp = 'AccZ'
+    ptch = 'EulerY'
+    racc = rdata[comp].values
+    lacc = ldata[comp].values #input AccZ values!
+    rpitch = rdata[ptch].values
+    lpitch = ldata[ptch].values
+    ph = Body_Phase(racc, lacc, rpitch, lpitch,  sampl_rate)
+    
+    rdata['Phase'] = ph
+    ldata['Phase'] = ph
+    hdata['Phase'] = ph
+        
+    lf_impact = impact_phase(ldata['AccZ'], sampl_rate)
+    rf_impact = impact_phase(rdata['AccZ'], sampl_rate)
+    rdata['Impact'] = rf_impact
+    ldata['Impact'] = lf_impact
+        
+    mass = 75
+    extra_mass = 0
+    
+    #Balance CME thresdholds
+    cme_dict = {'prosupl':[-1, -4, 2, 8], 'hiprotl':[-1, -4, 2, 8], 'hipdropl':[-1, -4, 2, 8],
+                'prosupr':[-1, -4, 2, 8], 'hiprotr':[-1, -4, 2, 8], 'hipdropr':[-1, -4, 2, 8],
+                'hiprotd':[-1, -4, 2, 8]}
+    #Impact CME thresholds
+    cme_dict_imp = {'landtime':[0.2, 0.25], 'landpattern':[12, -50]}
+
+    neutral_h = np.matrix([0.582,0.813,0,0]) # will come from anatomical_fix module as such anatom.neutral_hq
+    neutral_l = np.matrix([0.582,0.813,0,0]) # will come from anatomical_fix module as such anatom.neutral_lq
+    neutral_r = np.matrix([0.582,0.813,0,0]) # will come from anatomical_fix module as such anatom.neutral_rq
+    #get "neutral" euler angles from quaternions    
+    neutral_eulh = prep.Calc_Euler(neutral_h)
+    neutral_eull = prep.Calc_Euler(neutral_l)
+    neutral_eulr = prep.Calc_Euler(neutral_r)
+    
+    #Contralateral Hip Drop
+    nr_contra = cmed.cont_rot_CME(hdata['EulerY'], rdata['Phase'], [2,0], neutral_eulh[1], cme_dict['hipdropr'])
+    nl_contra = cmed.cont_rot_CME(hdata['EulerY'], rdata['Phase'], [1,0], neutral_eulh[1], cme_dict['hipdropl'])
+    #Pronation/Supination
+    nr_prosup = cmed.cont_rot_CME(rdata['EulerX'], rdata['Phase'], [2,0], neutral_eulr[0], cme_dict['prosupr'])
+    nl_prosup = cmed.cont_rot_CME(ldata['EulerX'], rdata['Phase'], [1,0], neutral_eull[0], cme_dict['prosupl'])
+    #Lateral Hip Rotation
+    nr_hiprot = cmed.cont_rot_CME(hdata['EulerZ'], rdata['Phase'], [2], neutral_eulh[2], cme_dict['hiprotr'])
+    nrdbl_hiprot = cmed.cont_rot_CME(hdata['EulerZ'], rdata['Phase'], [0], neutral_eulh[2], cme_dict['hiprotd'])
+    nl_hiprot = cmed.cont_rot_CME(hdata['EulerZ'], rdata['Phase'], [1], neutral_eulh[2], cme_dict['hiprotl'])
+    nldbl_hiprot = cmed.cont_rot_CME(hdata['EulerZ'], rdata['Phase'], [0], neutral_eulh[2], cme_dict['hiprotd'])
+    
+    #Landing Time
+    n_landtime = sync_time(rdata['Impact'], ldata['Impact'], sampl_rate, cme_dict_imp['landtime'])
+    #Landing Pattern
+    if len(n_landtime) != 0:
+        n_landpattern = landing_pattern(rdata['EulerY'], ldata['EulerY'], n_landtime[:,0], n_landtime[:,1], cme_dict_imp['landpattern'])
+    else:
+        n_landpattern = np.array([])
+            
+    ldd = load_bal_imp(rdata, ldata, hdata, mass, extra_mass)
+
+    score = weight_load(nr_contra, nl_contra, nr_prosup, nl_prosup, nr_hiprot, nl_hiprot, nrdbl_hiprot, nldbl_hiprot,n_landtime, n_landpattern, ldd)
+    
+    print score
