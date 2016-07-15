0e2d817b0d00f5b553e544acd5c976a0ab3ebb8a
brian-biometrix <brian@biometrixtech.com>
2016-07-14 22:46:37 -0400

    added changes that account for foot orientation.

--- app/src/Under_Construction/balancePhase_v1.py
+++ app/src/Under_Construction/balancePhase_v1.py
@@ -8,27 +8,18 @@ Created on Tue Jul 12 12:00:16 2016
 import numpy as np
 import matplotlib.pyplot as plt
 
-rpath = 'C:\\Users\\Ankur\\python\\Biometrix\\Data analysis\\data exploration\\data files\\Subject5\\Subject5_rfdatabody_LESS.csv'
-#rpath = 'C:\\Users\\Ankur\\python\\Biometrix\\Data analysis\\data exploration\\data files\\ChangeDirection\\Rheel_Gabby_changedirection_set1.csv'
-#rpath = 'C:\Users\Ankur\python\Biometrix\Data analysis\data exploration\data files\Walking\Rheel_Gabby_walking_heeltoe_set1.csv'
-#lpath = 'C:\Users\Ankur\python\Biometrix\Data analysis\data exploration\data files\Walking\Lheel_Gabby_walking_heeltoe_set1.csv'   
-#lpath = 'C:\Users\Ankur\python\Biometrix\Data analysis\data exploration\data files\Subject5\Subject5_lfdatabody_set1.csv'
-#lpath = 'C:\Users\Ankur\python\Biometrix\Data analysis\data exploration\data files\Stomp\Lheel_Gabby_stomp_set1.csv'
-#lpath = 'C:\\Users\\Ankur\\python\\Biometrix\\Data analysis\\data exploration\\data files\\ChangeDirection\\Lheel_Gabby_changedirection_set1.csv'
-lpath = 'C:\\Users\\Ankur\\python\\Biometrix\\Data analysis\\data exploration\\data files\\Subject5\Subject5_lfdatabody_LESS.csv'
-#lpath = 'C:\Users\Ankur\python\Biometrix\Data analysis\data exploration\data files\Jump\Lheel_Gabby_jumping_explosive_set2.csv'
-#lpath = 'C:\Users\Ankur\python\Biometrix\Data analysis\data exploration\data files\Walking\Lheel_Gabby_walking_heeltoe_set1.csv'
-#hpath = 'C:\Users\Ankur\python\Biometrix\Data analysis\data exploration\data files\Subject5\Subject5_hipdatabody_set1.csv'
-hpath = 'C:\\Users\\Ankur\\python\\Biometrix\\Data analysis\\data exploration\\data files\\Subject5\\Subject5_hipdatabody_LESS.csv'
+#rpath = 'C:\\Users\\Brian\\Documents\\Biometrix\\Data\\Collected Data\\PhaseDetect\\Subject5_rfdatabody_LESS.csv'
+#lpath = 'C:\\Users\\Brian\\Documents\\Biometrix\\Data\\Collected Data\\PhaseDetect\\Subject5_lfdatabody_LESS.csv'
+#hpath = 'C:\\Users\\Brian\\Documents\\Biometrix\\Data\\Collected Data\\PhaseDetect\\Subject5_hipdatabody_LESS.csv'
+
+rpath = 'C:\\Users\\Brian\\Documents\\Biometrix\\Data\\Collected Data\\PhaseDetect\\Rheel_Gabby_changedirection_set1.csv'
+lpath = 'C:\\Users\\Brian\\Documents\\Biometrix\\Data\\Collected Data\\PhaseDetect\\Lheel_Gabby_changedirection_set1.csv'
+hpath = 'C:\\Users\\Brian\\Documents\\Biometrix\\Data\\Collected Data\\PhaseDetect\\hips_Gabby_changedirection_set1.csv'
+
 
 rdata = np.genfromtxt(rpath, delimiter = ",", dtype = float, names = True)
 ldata = np.genfromtxt(lpath, delimiter = ",", dtype = float, names = True)
 hdata = np.genfromtxt(hpath, delimiter = ",", dtype = float, names = True)
-
-plt.figure(1)
-#plt.plot(ldata['AccX'])
-plt.plot(rdata['AccX'])
-plt.show()
 
 # In[]:
 
@@ -38,6 +29,9 @@ import pandas as pd
 
 acc = 0
 acc = rdata['AccX']
+orient = rdata['EulerY']
+#acc = rdata['AccZ']
+#acc = np.sqrt(rdata['AccX']**2+.1*rdata['AccZ']**2)
 std_acc = 0
 mean_acc = 0
 
@@ -45,24 +39,18 @@ mean_acc = 0
 hz = 100
 win = int(0.2*hz)
 
-#when sampling rate is 250Hz
-hz = 250
-win = int(0.4*hz)
+##when sampling rate is 250Hz
+#hz = 250
+#win = int(0.4*hz)
 
-#a = acc[:win]
+
 mean_acc = pd.rolling_mean(acc, window = win, center=True)
 std_acc = pd.rolling_std(acc, window = win, center=True)
+std_orient = pd.rolling_std(orient, window=win, center=True)
 sum_mean_std = [ i + j for i,j in zip(mean_acc, std_acc) ]
 dist_sum_mean = np.array([ abs(i-j) for i,j in zip(mean_acc, sum_mean_std) ])
 dist_sum_mean[np.isnan(dist_sum_mean)] = 0
 
-print(dist_sum_mean, len(dist_sum_mean), len(acc))
-
-plt.figure(2)
-plt.plot(mean_acc)
-plt.plot(sum_mean_std)
-plt.plot(rdata['AccZ'])
-plt.show()
 
 # In[]:
 
@@ -75,11 +63,10 @@ for i in range(len(dist_sum_mean)-5):
             count = count + 1
     if count == 5:
         for k in range(5):
-            dummy_start_bal.append(i+k)
+            if orient[i] < .3 and std_orient[i] < .1:
+                dummy_start_bal.append(i+k)
             
 start_bal = np.unique(dummy_start_bal)
-        
-print(start_bal)
 
 
 # In[]:
@@ -91,10 +78,13 @@ for i in start_bal:
     bal_phase[i] = 0
     
 #This is the end of the algorithm to determine the balance phase
-    
+up = 500
+down = 950    
 plt.figure(3)
-plt.plot(bal_phase)
-plt.plot(rdata['AccZ'])
+plt.plot(bal_phase[up:down])
+plt.plot(rdata['AccZ'][up:down])
+#plt.plot(std_acc[up:down])
+plt.plot(std_orient[up:down])
 plt.show()
 
 # In[]:
