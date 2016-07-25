7e659bf236ae02b3fcd93830b0c0677342543aa4
brian-biometrix <brian@biometrixtech.com>
2016-07-25 14:04:59 -0400

    now uses built in to_array fxn.

--- app/src/Under_Construction/do_tocsv.py
+++ app/src/Under_Construction/do_tocsv.py
@@ -9,19 +9,6 @@ import setUp as su
 import coordinateFrameTransformation as prep
 import numpy as np
 
-def stack_data(data):
-    output = np.column_stack((data.qW, data.qX))
-    output = np.column_stack((output, data.qY))
-    output = np.column_stack((output, data.qZ))
-    output = np.column_stack((output, data.AccX))
-    output = np.column_stack((output, data.AccY))
-    output = np.column_stack((output, data.AccZ))
-    output = np.column_stack((output, data.EulerX))
-    output = np.column_stack((output, data.EulerY))
-    output = np.column_stack((output, data.EulerZ))
-    
-    return output
-
 def run_analytics(path, mass, extra_mass, hz, anatom=None):
     
     #Set up objects that hold raw datasets 
@@ -30,15 +17,15 @@ def run_analytics(path, mass, extra_mass, hz, anatom=None):
     #Create Inertial Frame objects transformed data
     #hip databody
     hipbf = prep.TransformData(data.hipdataset)
-    hipbf = stack_data(hipbf)   
+    hipbf = hipbf.to_array()  
     
     #rfoot databody
     rfbf = prep.TransformData(data.rfdataset)
-    rfbf = stack_data(rfbf)
+    rfbf = rfbf.to_array() 
     
     #lfoot databody
     lfbf = prep.TransformData(data.lfdataset)
-    lfbf = stack_data(lfbf)
+    lfbf = lfbf.to_array()
     
     return hipbf, rfbf, lfbf
     
@@ -47,7 +34,7 @@ if __name__ == '__main__':
     import pandas as  pd
     import matplotlib.pyplot as plt
     
-    path = 'C:\\Users\\Brian\\Documents\\Biometrix\\Data\\Collected Data\\Fixed_By_Exercise\\Subject1_DblSquat.csv'
+    path = 'C:\\Users\\Brian\\Documents\\Biometrix\\Data\\Collected Data\\Fixed_Trial4\\SensorData_062816_anacalib_dblsquat_set1.csv'
 
     hipbf, rfbf, lfbf = run_analytics(path, 75, 25, 250)
     final = np.concatenate((lfbf, hipbf), axis=1)
@@ -57,7 +44,12 @@ if __name__ == '__main__':
                                       'HqW', 'HqX', 'HqY', 'HqZ', 'HAccX', 'HAccY','HAccZ', 'HEulerX', 'HEulerY', 'HEulerZ',
                                       'RqW', 'RqX', 'RqY', 'RqZ', 'RAccX', 'RAccY','RAccZ', 'REulerX', 'REulerY', 'REulerZ'])
     
-    df.to_csv('C:\\Users\\Brian\\Documents\\Biometrix\\Data\\Collected Data\\Fixed_By_Exercise\\test.csv')
+    df.to_csv('C:\\Users\\Brian\\Documents\\Biometrix\\Data\\Collected Data\\Fixed_Trial4\\test.csv')
+    
+    plt.plot(df['LEulerX'])
+    plt.plot(df['LEulerY'])
+    plt.plot(df['LEulerZ'])
+    plt.show()
     
     
     
