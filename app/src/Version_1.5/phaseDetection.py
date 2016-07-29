c91509ce642286e4f26500a0ca975b4620159228
brian-biometrix <brian@biometrixtech.com>
2016-07-28 23:43:53 -0400

    now includes new balance phase detection algo.

--- app/src/Version_1.5/phaseDetection.py
+++ app/src/Version_1.5/phaseDetection.py
@@ -8,99 +8,61 @@ Created on Thu Jul 07 16:27:47 2016
 """
 #############################################INPUT/OUTPUT####################################################
 Function: combine_phase
-Inputs: AccZ right and left feet; EulerY angles for right and left feet; sampling rate
+Inputs: AccX, AccZ right and left feet; EulerY angles for right and left feet; sampling rate
 Outputs: 2 arrays; left foot phase; right foot phase
 #############################################################################################################
 """
 
 import numpy as np
 import pandas as pd
-import setUp as su
-from itertools import islice
 from phaseID import phase_id
 
-def Move(std, pitch, w): #inputs array of st. devs
-    infl = .0001 #determines how sticky you want the mean and std to be
-    new_u = np.mean(std[int(.5*w):int(1.5*w)]) #determine initial mean
-    #print(new_u)
-    new_std = np.std(std[int(.5*w):int(1.5*w)]) #determine initial mean
-    store = [0]*w #initialize list with first 2*w terms = 0    
-    for i in range(w, len(std)):
-        if std[i] > new_u + 1*new_std and std[i] >1.2: #if data point exceeds 1 SD and is great than 1.2
-            new_u = (new_u + infl*std[i])/(1+infl) #find new mean
-            new_std = (new_std + infl*(np.sqrt((std[i]-new_u)**2))/(1+infl)) #find new SD
-            store.append(1) #add to list
-        else:
-            new_u = (new_u + infl*std[i])/(1+infl) #find new mean
-            new_std = (new_std + infl*(np.sqrt((std[i]-new_u)**2)))/(1+infl) #find new SD
-            if pitch[i] >= .9: 
-                store.append(1) #add to list
-            else:
-                store.append(0)
-    return store
-
-def Grad_Move(u, pitch, w): #inputs array of data points
-    infl = .00015 #determines how sticky you want mean and std to be
-    
-    new_u = np.mean(u[int(.5*w):int(1.5*w)]) #determine initial mean
-    new_std = np.std(u[int(.5*w):int(1.5*w)]) #determine initial mean
-    store = [0]*w  #initialize list with first 2*w terms = 0  
-    for i in range(w, len(u)):
-        if (u[i] > new_u + 1*new_std or u[i] < new_u - 1*new_std) and abs(u[i]) > 1.5:#if data point exceeds 1 SD and is great than 1.5
-            new_u = (new_u + infl*u[i])/(1+infl) #find new mean
-            new_std = (new_std + infl*(np.sqrt((u[i]-new_u)**2))/(1+infl)) #find new SD
-            store.append(1) #add to list
-        else:
-            new_u = (new_u + infl*u[i])/(1+infl) #find new mean
-            new_std = (new_std + infl*(np.sqrt((u[i]-new_u)**2)))/(1+infl) #find new SD
-            if pitch[i] >= .9: 
-                store.append(1) #add to list
-            else:
-                store.append(0)
-    return store
-
-def Comb_Move(move, gmove):
-    lst = [] #initiate list
-    for i in range(len(move)):
-        if move[i] == 1 or gmove[i] == 1: #if either move functions = 1 then mark as moving
-            lst.append(1) #add to list
-        else:
-            lst.append(0) #if neither function = 1 dont mark as moving
-    return np.array(lst)
-
-def Final(mscore):
-    lst = [] #initiate list
-    for i in range(len(mscore)):
-        if mscore[i] > 0: #if score is greater than 0 mark as moving
-            lst.append(1) #add to list
-        else:
-            lst.append(0) #marked as not moving
-    return np.array(lst)
-
-def Fix_Edges(df, edge):
-    for i in range(1, len(df)):
-        if df[i]-df[i-1] < 0: #if point is moving to non-moving fix right edge of moving region
-            df[i-edge:i] = 0 #adjust array
-        else:
-            None
-    return df
-
 def Phase_Detect(series, pitch, hz):
-    w = int(.08*hz) #define rolling mean and st dev window
-    edge = int(.2*hz) #define window to average moving decisions over
-    uaZ = pd.rolling_mean(series, window=w, center=True) #take rolling mean
-    stdaZ = pd.rolling_std(series, window=w, center=True) #take rolling st dev
     
-    move = Move(stdaZ, pitch, w) #determine if there is sudden move in data
-    gmove = Grad_Move(uaZ, pitch, w) #determine if there is gradual move in data
-    cmove = Comb_Move(move, gmove) #combine two types of moves
-    mscore = pd.rolling_mean(cmove, window=edge) #take rolling mean of moves to handle discontinuities
-    trans = Final(mscore) #determine if in data point is in moving phase
-    final = Fix_Edges(trans, edge) #fix right edge since rolling mean wrongly extends moving regions
+    acc = 0
+    acc = abs(series['AccX']) #absolute value of AccX
+    orient = abs(pitch) #absolute value of EulerY
+    accdiff = abs(series['AccZ']-acc) #absolute value of the difference between AccZ and AccX
+    std_acc = 0    
     
-    return final #return array
+    #setting the rolling window based on the sampling rate
+    if hz == 250:
+        win = int(0.4*hz) #rolling window
+    elif hz == 100:
+        win = int(0.2*hz) #rolling window
+        
+    consec_pnts = int(0.02*hz) #number of consecutive points to be considered to test for balance phase condition
+    mean_accdiff = pd.rolling_mean(accdiff, window=win, center=True) #rolling mean of the difference between AccZ and AccX
+    std_acc = pd.rolling_std(acc, window = win, center=True) #rolling std of absolute values of AccX
+    std_orient = pd.rolling_std(orient, window=win, center=True) #rolling std of absolute values of EulerY
+    
+    dummy_start_bal = []
+    start_bal = []
+
+    for i in range(len(std_acc)-consec_pnts):
+        count = 0
+        for j in range(consec_pnts): 
+            if 0 < std_acc[i+j] <= 1.1:
+                count = count + 1
+        if count == consec_pnts: #if count satisfies the minimum number of consecutive data points required to satisfy the balance phase conditions
+            for k in range(consec_pnts):
+                if std_orient[i+k] < .07 and orient[i+k] < .9:
+                    dummy_start_bal.append(i+k)
+                if orient[i+k] > .3 and mean_accdiff[i+k] > 2 and i+k in dummy_start_bal:
+                    dummy_start_bal.remove(i+k)
+            
+    start_bal = np.unique(dummy_start_bal)
+    
+    bal_phase = []
+    bal_phase = [1]*len(acc)
+
+    for i in start_bal:
+        bal_phase[i] = 0
+    
+    return np.array(bal_phase) #return array
 
 def Body_Phase(right, left, rpitch, lpitch, hz):
+    
     r = Phase_Detect(right, rpitch, hz) #run phase detect on right foot
     l = Phase_Detect(left, lpitch, hz) #run phase detect on left foot
     
@@ -138,6 +100,9 @@ def bound_det_lf(p):
             end_move.append(i)
         elif p[i] == phase_id.rflf_offground.value and p[i+1] == phase_id.lf_ground.value:
             end_move.append(i)
+            
+    start_move = np.delete(start_move, [len(start_move)-1])
+    end_move = np.delete(end_move, [0])
                         
     return start_move, end_move
     
@@ -164,6 +129,9 @@ def bound_det_rf(p):
         elif p[i] == phase_id.rflf_offground.value and p[i+1] == phase_id.rf_ground.value:
             end_move.append(i)
             
+    start_move = np.delete(start_move, [len(start_move)-1])
+    end_move = np.delete(end_move, [0])
+            
     return start_move, end_move
     
 def impact_detect(start_move, end_move, az, hz):
@@ -175,10 +143,13 @@ def impact_detect(start_move, end_move, az, hz):
     acc = 0
     start_imp = []
     end_imp = []
-
+    
+    print(start_move, end_move)
+        
     for i,j in zip(start_move, end_move):
         arr_len = []
         dummy_start_imp = []
+        dummy_end_imp = []
         acc = az[i:j]
         arr_len = range(len(acc)-win)
         numbers = iter(arr_len)
@@ -187,26 +158,27 @@ def impact_detect(start_move, end_move, az, hz):
                 for l in range(win):
                     if acc[k+l] >= pos_thresh:
                         dummy_start_imp.append(i+k)
+                        dummy_end_imp.append(i+k+l)
                         break
-                next(islice(numbers, win, 1 ), None) #skip 0.05*hz data points in the second 'for' loop
         if len(dummy_start_imp) == 1:
             start_imp.append(dummy_start_imp[0])
             end_imp.append(j)
-        if len(dummy_start_imp) > 1:
-            for m in range(len(dummy_start_imp)):
+        elif len(dummy_start_imp) > 1:
+            for m, n in zip(range(len(dummy_start_imp)), range(len(dummy_end_imp))):
                 if (((j-i)/2)+i) < dummy_start_imp[m] <= j:
-                    start_imp.append(dummy_start_imp[m])
+                    pos = np.argmin(az[dummy_start_imp[m]:dummy_end_imp[n]]) #returns the positin of the minimum acceleration value
+                    start_imp.append(pos+dummy_start_imp[m])
                     end_imp.append(j)
                     break
                 
     imp = []
     imp = [ [i,j] for i,j in zip(start_imp, end_imp) ]
-    
+            
     return np.array(imp)
 
-def combine_phase(laccz, raccz, rpitch, lpitch, hz):
+def combine_phase(lacc, racc, rpitch, lpitch, hz):
     
-    ph = Body_Phase(raccz, laccz, rpitch, lpitch, hz) #balance phase for both the right and left feet
+    ph = Body_Phase(racc, lacc, rpitch, lpitch, hz) #balance phase for both the right and left feet  
     
     lf_ph = list(ph)
     rf_ph = list(ph)
@@ -214,23 +186,14 @@ def combine_phase(laccz, raccz, rpitch, lpitch, hz):
     lf_sm, lf_em = bound_det_lf(lf_ph) #detecting the start and end points of the left foot movement phase
     rf_sm, rf_em = bound_det_rf(rf_ph) #detecting the start and end points of the right foot movement phase
     
-    lf_imp = impact_detect(lf_sm, lf_em, laccz, hz) #starting and ending point of the impact phase for the left foot
-    rf_imp = impact_detect(rf_sm, rf_em, raccz, hz) #starting and ending points of the impact phase for the right foot
+    lf_imp = impact_detect(lf_sm, lf_em, lacc['AccZ'], hz) #starting and ending point of the impact phase for the left foot
+    rf_imp = impact_detect(rf_sm, rf_em, racc['AccZ'], hz) #starting and ending points of the impact phase for the right foot
 
-    if len(lf_imp) == 0 and len(rf_imp) == 0:
-        pass
-    elif len(lf_imp) == 0:
-        for x,y in zip(rf_imp[:,0], rf_imp[:,1]):
-            rf_ph[x:y] = [phase_id.rf_imp.value]*int(y-x) #decide impact phase for the right foot 
-    elif len(rf_imp) == 0:
-        for i,j in zip(lf_imp[:,0], lf_imp[:,1]):
-            lf_ph[i:j] = [phase_id.lf_imp.value]*int(j-i) #decide impact phase for the left foot
-    else:
-        for i,j in zip(lf_imp[:,0], lf_imp[:,1]):
-            lf_ph[i:j] = [phase_id.lf_imp.value]*int(j-i) #decide impact phase for the left foot
-        
-        for x,y in zip(rf_imp[:,0], rf_imp[:,1]):
-            rf_ph[x:y] = [phase_id.rf_imp.value]*int(y-x) #decide impact phase for the right foot
+    for i,j in zip(lf_imp[:,0], lf_imp[:,1]):
+        lf_ph[i:j] = [phase_id.lf_imp.value]*int(j-i) #decide impact phase for the left foot
+    
+    for x,y in zip(rf_imp[:,0], rf_imp[:,1]):
+        rf_ph[x:y] = [phase_id.rf_imp.value]*int(y-x) #decide impact phase for the right foot            
             
     return np.array(lf_ph), np.array(rf_ph)
     
@@ -238,18 +201,18 @@ if __name__ == "__main__":
     
     import matplotlib.pyplot as plt
     
-    #rpath = 'C:\\Users\\Ankur\\python\\Biometrix\\Data analysis\\data exploration\\data files\\Subject5\\Subject5_rfdatabody_LESS.csv'
-    #rpath = 'C:\Users\Ankur\python\Biometrix\Data analysis\data exploration\data files\ChangeDirection\Rheel_Gabby_changedirection_set1.csv'
+    rpath = 'C:\\Users\\Ankur\\python\\Biometrix\\Data analysis\\data exploration\\data files\\Subject5\\Subject5_rfdatabody_LESS.csv'
+    #rpath = 'C:\\Users\\Ankur\\python\\Biometrix\\Data analysis\\data exploration\\data files\\ChangeDirection\\Rheel_Gabby_changedirection_set1.csv'
     #rpath = 'C:\Users\Ankur\python\Biometrix\Data analysis\data exploration\data files\Walking\Rheel_Gabby_walking_heeltoe_set1.csv'
     #lpath = 'C:\Users\Ankur\python\Biometrix\Data analysis\data exploration\data files\Walking\Lheel_Gabby_walking_heeltoe_set1.csv'   
     #lpath = 'C:\Users\Ankur\python\Biometrix\Data analysis\data exploration\data files\Subject5\Subject5_lfdatabody_set1.csv'
     #lpath = 'C:\Users\Ankur\python\Biometrix\Data analysis\data exploration\data files\Stomp\Lheel_Gabby_stomp_set1.csv'
-    #lpath = 'C:\Users\Ankur\python\Biometrix\Data analysis\data exploration\data files\ChangeDirection\Lheel_Gabby_changedirection_set1.csv'
-    #lpath = 'C:\\Users\\Ankur\\python\\Biometrix\\Data analysis\\data exploration\\data files\\Subject5\Subject5_lfdatabody_LESS.csv'
+    #lpath = 'C:\\Users\\Ankur\\python\\Biometrix\\Data analysis\\data exploration\\data files\\ChangeDirection\\Lheel_Gabby_changedirection_set1.csv'
+    lpath = 'C:\\Users\\Ankur\\python\\Biometrix\\Data analysis\\data exploration\\data files\\Subject5\Subject5_lfdatabody_LESS.csv'
     #lpath = 'C:\Users\Ankur\python\Biometrix\Data analysis\data exploration\data files\Jump\Lheel_Gabby_jumping_explosive_set2.csv'
     #lpath = 'C:\Users\Ankur\python\Biometrix\Data analysis\data exploration\data files\Walking\Lheel_Gabby_walking_heeltoe_set1.csv'
     #hpath = 'C:\Users\Ankur\python\Biometrix\Data analysis\data exploration\data files\Subject5\Subject5_hipdatabody_set1.csv'
-    #hpath = 'C:\\Users\\Ankur\\python\\Biometrix\\Data analysis\\data exploration\\data files\\Subject5\\Subject5_hipdatabody_LESS.csv'
+    hpath = 'C:\\Users\\Ankur\\python\\Biometrix\\Data analysis\\data exploration\\data files\\Subject5\\Subject5_hipdatabody_LESS.csv'
 
     rdata = np.genfromtxt(rpath, delimiter = ",", dtype = float, names = True)
     ldata = np.genfromtxt(lpath, delimiter = ",", dtype = float, names = True)
@@ -266,18 +229,13 @@ if __name__ == "__main__":
     rpitch = rdata[ptch]
     lpitch = ldata[ptch]
     #ph = Body_Phase(racc, lacc, rpitch, lpitch, sampl_rate)
+
+    lf_phase, rf_phase = combine_phase(ldata[['AccX', 'AccZ']], rdata[['AccX', 'AccZ']], rpitch, lpitch, sampl_rate)
     
-    lf_phase, rf_phase = combine_phase(ldata['AccZ'], rdata['AccZ'], rpitch, lpitch, sampl_rate)
+    #Plotting
     
-    ###Plotting
-    up = 0
-    down = len(rdata)
-    
-    aseries = ldata[up:down]
-    #indic = phase[up:down]
-    
-    plt.figure(5)    
-    plt.plot(lf_phase)
-    plt.plot(ldata['AccZ'])
+    plt.figure(1)    
+    plt.plot(rf_phase)
+    plt.plot(rdata['AccZ'])
     #plt.title(comp)
     plt.show()
