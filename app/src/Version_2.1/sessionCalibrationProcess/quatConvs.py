605788c7d381fb405bd70b0b295a594bee2d9101
courtney-white <courtney@biometrixtech.com>
2016-11-29 13:56:06 -0500

    quatConvs with singularity handling added to lambda processes

--- app/src/Version_2.1/sessionCalibrationProcess/quatConvs.py
+++ app/src/Version_2.1/sessionCalibrationProcess/quatConvs.py
@@ -8,59 +8,90 @@ Created on Wed Sep 28 17:42:39 2016
 # import relevant packages
 import numpy as np
 import quatOps as qo
-
+import warnings
+from decimal import *
+getcontext().prec = 40
 """
-#########################################################################################
-Conversions between quaternions and Euler angles, using embedded direction cosine matrices
+############################################################################
+Conversions between quaternions and Euler angles, using embedded direction
+cosine matrices
 
 Conceptual explanations to be given under:
-https://sites.google.com/a/biometrixtech.com/wiki/home/preprocessing/anatomical/concepts
+https://sites.google.com/a/biometrixtech.com/wiki/home/preprocessing/
+    anatomical/concepts
 Further documentation of code to be given under:
 https://drive.google.com/drive/folders/0Bzd7PD0NIJ7ZZmJ5aVRsUnVOZ3c
-#########################################################################################
+############################################################################
 """
+
+
+warnings.filterwarnings("error")
 
 
 def quat_to_euler(q):
     """Function that transforms quaternion into Euler angles, assuming ZYX
     config.
-    
+
     Args:
         q: single quaternion
-    
+
     Returns:
-        3 euler angles measuring rotaion about  z(phi), y(theta) and x(psi) axes   
+        3 euler angles measuring rotaion about
+        z(phi), y(theta) and x(psi) axes
+
     """
-    
-#    q= q.tolist()
+    if np.isnan(q).any():
+        return np.array([[np.nan,np.nan,np.nan]])
+
     q = qo.quat_norm(q)
-    
+
     # calculate relevant elements of direction cosine matrix
-    a = 2.*q[:, 0]**2 - 1. + 2.*q[:, 1]**2
-    b = 2.*(q[:, 1]*q[:, 2] - q[:, 0]*q[:, 3])
-    c = 2.*(q[:, 1]*q[:, 3] + q[:, 0]*q[:, 2])
-    d = 2.*(q[:, 2]*q[:, 3] - q[:, 0]*q[:, 1])
-    e = 2.*q[:, 0]**2 - 1. + 2.*q[:, 3]**2
-    
-    # calculate euler angles from direction cosine matrix components
+    a = 2*q[:, 0]**2 - 1 + 2*q[:, 1]**2
+    b = 2*(q[:, 1]*q[:, 2] - q[:, 0]*q[:, 3])
+    c = 2*(q[:, 1]*q[:, 3] + q[:, 0]*q[:, 2])
+    d = 2*(q[:, 2]*q[:, 3] - q[:, 0]*q[:, 1])
+    e = 2*q[:, 0]**2 - 1 + 2*q[:, 3]**2
+    c[c >1] = 1
     phi = np.arctan2(d, e)
-    C = c/np.sqrt(np.abs(1 - c**2))
-    theta = -np.arctan(C)
+    theta = -np.arcsin(c)
     psi = np.arctan2(b, a)
-    
-    return np.vstack([phi,theta,psi]).T
-  
+    if any(c > .999999999):
+        q1 = q[c > .999999999]
+        phi[c > .999999999] = 0
+        theta[c > .999999999] = -np.pi/2
+        psi[c > .999999999] = np.arctan2(-2*(q1[:, 1]*q1[:, 2] + q1[:, 0] \
+                                         *q1[:, 3]),
+                         -2*(q1[:, 1]*q1[:, 3] - q1[:, 0]*q1[:, 2]))
+    elif any(c < -.999999999):
+        q1 = q[c < -.999999999]
+        phi[c < -.999999999] = 0
+        theta[c < -.999999999] = np.pi/2
+        psi[c < -.999999999] = np.arctan2(2*(q1[:, 1]*q1[:, 2] + q1[:, 0] \
+                                          *q1[:, 3]), 2*(q1[:, 1]*q1[:, 3] \
+                                          - q1[:, 0]*q1[:, 2]))
+    elif any(np.sum(np.abs(q - np.array([[0, 0, 1, 0]])) \
+             < np.array([[1e-8]*4]), axis=1) == 4):
+        ind = np.sum(np.abs(q - np.array([[0, 0, 1, 0]])) \
+                     < np.array([[1e-8]*4]), axis=1) == 4
+        phi[ind] = 0
+        theta[ind] = np.pi
+        psi[ind] = 0
+
+    return np.vstack([phi, theta, psi]).T
+
+
 def euler_to_quat(euler_data):
     """Function that transforms set of Euler angles into quaternion, assuming
     ZYX config.
+
     Args:
         phi: euler angle measuring rotaion about z axis
         theta: euler angle measuring rotaion about y axis
         psi: euler angle measuring rotaion about x axis
-        
+
     Returns:
         single (WXYZ) quaternion transformation of given euler angles
-    
+
     """
     phi = euler_data[:, 0]
     theta = euler_data[:, 1]
@@ -93,20 +124,25 @@ def euler_to_quat(euler_data):
     N = (g-c)/3
     O = (b-d)/3
     P = (a+e+k)/3
-    
+
     # construct the quaternion matrix
     Q = np.array([[A,B,C,D],[E,F,G,H],[I,J,K,L],[M,N,O,P]])
     Q = Q.swapaxes(0,2)
-    
+
     # find the maximum eigenvalue of the quaternion matrix
     [D,V] = np.linalg.eig(Q)
+
     max_eig=np.argmax(D,1)
-    # find the eigenvector containing the largest eigenvalue and extract the 
-    # quaternion from its components.
+    # find the eigenvector containing the largest eigenvalue and extract the
+        # quaternion from its components.
     q = np.zeros((len(max_eig), 4))
+
     for row in range(len(max_eig)):
-        q[row,:] =  V[row,:,max_eig[row]]
+        try:
+            q[row,:] =  V[row,:,max_eig[row]]
+        except Warning:
+            warnings.filterwarnings("ignore")
+            q[row,:] =  V[row,:,max_eig[row]]
     q = np.vstack([q[:,3],q[:,0],q[:,1],q[:,2]]).T
-    
+
     return q
-    
