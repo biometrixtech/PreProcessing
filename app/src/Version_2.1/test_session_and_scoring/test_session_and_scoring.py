9673f000796404e0cc513b08cbb7f4e215421cb1
Dipesh Gautam <dipesh@biometrixtech.com>
2017-01-02 13:43:43 -0500

    Update: Added checks for data write to S3 and DB for session/scoring

--- app/src/Version_2.1/test_session_and_scoring/test_session_and_scoring.py
+++ app/src/Version_2.1/test_session_and_scoring/test_session_and_scoring.py
@@ -9,6 +9,9 @@ import sys
 import unittest
 from pyramid import testing
 import psycopg2
+import boto3
+import cStringIO
+import pandas as pd
 
 sys.path.insert(0, '..\\sessionProcess')
 from runAnalytics import run_session
@@ -22,7 +25,9 @@ conn = psycopg2.connect("""dbname='biometrix' user='ubuntu'
 host='ec2-35-162-107-177.us-west-2.compute.amazonaws.com' 
 password='d8dad414c2bb4afd06f8e8d4ba832c19d58e123f'""")
 cur = conn.cursor()
-
+S3 = boto3.resource('s3')
+cont_scoring = 'biometrix-scoringcontainer'
+cont_sess = 'biometrix-sessionprocessedcontainer'
 
 class TestSessionAndScoring(unittest.TestCase):
     """Tests for Session and Scoring processes
@@ -62,12 +67,53 @@ class TestSessionAndScoring(unittest.TestCase):
     # Testing for expected test case
     def test_session_and_scoring_happy_path(self):
         sensor_data = "dipesh_merged_II.csv"
+        data = pd.read_csv(sensor_data)
         file_name = "46d2f70d-7866-41a0-aae4-e5478ae9d4f3"
         response = run_session(sensor_data, file_name)
-
         #Assert the process ran successfully!
         self.assertEqual(response, "Success!")
 
+        #Assert file was written to scoring container
+        files_scoring = []
+        for obj in S3.Bucket(cont_scoring).objects.all():
+            files_scoring.append(obj.key)
+        self.assertIn(file_name, files_scoring)
+
+
+        obj = S3.Bucket(cont_scoring).Object(file_name)
+        fileobj = obj.get()
+        body = fileobj["Body"].read()
+        scoring_data = cStringIO.StringIO(body)
+        response_scoring = run_scoring(scoring_data, file_name)
+        self.assertEqual(response_scoring, "Success!")
+
+        # Assert processed file and movement table written to processed cont
+        files_session_processed = []
+        for obj in S3.Bucket(cont_sess).objects.all():
+            files_session_processed.append(obj.key)
+        self.assertIn('processed_'+file_name, files_session_processed)
+        self.assertIn('movement_'+file_name, files_session_processed)
+
+
+        # Assert there's no missing data in movement table
+        quer_read_id = """select id from session_events where
+                            sensor_data_filename = %s"""
+        cur.execute(quer_read_id, (file_name,))
+        session_event_id = cur.fetchall()[0][0]
+        quer_read_mov = """select count(*) from movement
+                            where session_event_id = %s"""
+        cur.execute(quer_read_mov, (session_event_id,))
+        count = cur.fetchall()[0][0]
+        self.assertEqual(count, len(data))
+
+        #Remove file from scoringcontainer
+        S3.Object(cont_scoring, file_name).delete()
+        #Remove data from movement table
+        quer_delete = """delete from movement where session_event_id = %s"""
+        cur.execute(quer_delete, (session_event_id,))
+        conn.commit()
+        conn.close()
+        
 #        #Read from base_calibration_events and make sure values are as expected
 #        read_from_base = """select feet_processed_sensor_data_filename,
 #                            feet_success from base_anatomical_calibration_events
