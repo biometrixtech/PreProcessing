9c5848153a55e1269d01659b4d74b8e1bd66d5a8
courtney-white <courtney@biometrixtech.com>
2017-03-13 10:18:49 -0400

    Commented out placement checks, assuming proper placement

--- app/src/Version_2.1/sessionCalibrationProcess/placementCheck.py
+++ app/src/Version_2.1/sessionCalibrationProcess/placementCheck.py
@@ -29,35 +29,41 @@ def placement_check(left_acc, hip_acc, right_acc):
 
     """
 
-    # find mean acceleration values across time
-    _left_mean = np.nanmean(left_acc, 0)
-    _hip_mean = np.nanmean(hip_acc, 0)
-    _right_mean = np.nanmean(right_acc, 0)
+#    # find mean acceleration values across time
+#    _left_mean = np.nanmean(left_acc, 0)
+#    _hip_mean = np.nanmean(hip_acc, 0)
+#    _right_mean = np.nanmean(right_acc, 0)
 
     # find the maximum acceleration range experienced by each sensor
     _left_diff = np.nanmax(np.nanmax(left_acc, 0) - np.nanmin(left_acc, 0))
     _hip_diff = np.nanmax(np.nanmax(hip_acc, 0) - np.nanmin(hip_acc, 0))
     _right_diff = np.nanmax(np.nanmax(right_acc, 0) - np.nanmin(right_acc, 0))
 
-    # left x should be pos and y should be neg
-    if _left_mean[0] > 200 and _left_mean[1] < -200 and np.absolute(_left_mean[2]) < 400:
-        bad_left_placement = False
-    else:
-        bad_left_placement = True
+#    # left x should be pos and y should be neg
+#    if _left_mean[0] > 200 and _left_mean[1] < -200 and np.absolute(_left_mean[2]) < 400:
+#        bad_left_placement = False
+#    else:
+#        bad_left_placement = True
+#
+#    # hip y should be very neg and other axes minimally affected by grav
+#    if np.absolute(_hip_mean[0]) < 400 and _hip_mean[1] < -800 and np.absolute(_hip_mean[2]) < 500:
+#        bad_hip_placement = False
+#    else:
+#        bad_hip_placement = True
+#
+#    # right x should be eg and y should be neg
+#    if _right_mean[0] < -200 and _right_mean[1] < -200 and np.absolute(_right_mean[2]) < 400:
+#        bad_right_placement = False
+#    else:
+#        bad_right_placement = True
 
-    # hip y should be very neg and other axes minimally affected by grav
-    if np.absolute(_hip_mean[0]) < 400 and _hip_mean[1] < -800 and np.absolute(_hip_mean[2]) < 500:
-        bad_hip_placement = False
-    else:
-        bad_hip_placement = True
+    # Temporary removal of placement checks (March 13, 2017)
+    # TODO(Courtney): Automate placement checks
+    bad_left_placement = False
+    bad_hip_placement = False
+    bad_right_placement = False
 
-    # right x should be eg and y should be neg
-    if _right_mean[0] < -200 and _right_mean[1] < -200 and np.absolute(_right_mean[2]) < 400:
-        bad_right_placement = False
-    else:
-        bad_right_placement = True
-
-    # magnitude of maximum acceleration should always be less than 2 Gs
+    #  range of acceleration should always be less than 0.3 Gs
     if _left_diff > 300 or _hip_diff > 300 or _right_diff > 300:
         moving = True
     else:
