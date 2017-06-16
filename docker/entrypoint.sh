31c22af356c5726fb8044359e20f7c1caa3d6b1d
Stephen Poole <stephen@melon.software>
2017-06-16 13:55:56 +0100

    Get mount errors in logs

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 # Mount the EFS directory
-mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=10,retrans=2 efs.internal:/ /net/efs
+echo "Mounting NFS directory"
+mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=10,retrans=2 efs.internal:/ /net/efs 2>&1
 
 # Execute the CMD from your Dockerfile, i.e. "npm start"
+echo "Executing batch_entrypoint.py"
 python ./batch_entrypoint.py "$1" "$2" "$3"
