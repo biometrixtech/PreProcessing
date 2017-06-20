fb15983e2992f8418b5ddc5b2543a85fffd87873
Unknown <dipesh@biometrixtech.com>
2017-06-20 15:08:25 -0400

    Changes to mechStressTraining to match new grf model

--- app/src/Version_2.2/sessionProcess2/mechStressTraining.py
+++ app/src/Version_2.2/sessionProcess2/mechStressTraining.py
@@ -5,53 +5,110 @@ Created on Fri Oct 14 11:57:32 2016
 @author: Gautam
 """
 
-
 from __future__ import division
-import pickle
+
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import GradientBoostingRegressor
+from scipy.signal import butter, filtfilt
 
 
-def prepare_data(data, train=True):
+def prepare_data(data_in, sc):
     """Subsets and transforms the training data as well as define features
     to be used
     Args:
         Data: pandas dataframe or RawFrame object with the acceleration, euler
-        train: boolean to indicate if we're training or predicting on the data
-        Note: if train is True, data should contain grf data
+        sc: scaler model to scale data
     Returns:
         X : Predictors
         Y : Reponse (only if training)
     """
-    #Extract relevant variables
-    data_pd = pd.DataFrame()
-    data_pd['LaX'] = np.array(data.LaX.reshape(-1,))
-    data_pd['LaY'] = np.array(data.LaY)
-    data_pd['LaZ'] = np.array(data.LaZ)
-    data_pd['LeX'] = np.array(data.LeX)
-    data_pd['LeY'] = np.array(data.LeY)
-    data_pd['LeZ'] = np.array(data.LeZ)
-    data_pd['phase_lf'] = np.array(data.phase_lf)
-    data_pd['RaX'] = np.array(data.RaX)
-    data_pd['RaY'] = np.array(data.RaY)
-    data_pd['RaZ'] = np.array(data.RaZ)
-    data_pd['ReX'] = np.array(data.ReX)
-    data_pd['ReY'] = np.array(data.ReY)
-    data_pd['ReZ'] = np.array(data.ReZ)
-    data_pd['phase_rf'] = np.array(data.phase_rf)
-    data_pd['HaX'] = np.array(data.HaX)
-    data_pd['HaY'] = np.array(data.HaY)
-    data_pd['HaZ'] = np.array(data.HaZ)
-    data_pd['HeX'] = np.array(data.HeX)
-    data_pd['HeY'] = np.array(data.HeY)
-    data_pd['HeZ'] = np.array(data.HeZ)
+
+    data = pd.DataFrame()
+    data['LaX'] = np.array(data_in.LaX.reshape(-1,))
+    data['LaY'] = np.array(data_in.LaY)
+    data['LaZ'] = np.array(data_in.LaZ)
+    data['LeX'] = np.array(data_in.LeX)
+    data['LeY'] = np.array(data_in.LeY)
+    data['LeZ'] = np.array(data_in.LeZ)
+    data['phase_lf'] = np.array(data_in.phase_lf)
+    data['RaX'] = np.array(data_in.RaX)
+    data['RaY'] = np.array(data_in.RaY)
+    data['RaZ'] = np.array(data_in.RaZ)
+    data['ReX'] = np.array(data_in.ReX)
+    data['ReY'] = np.array(data_in.ReY)
+    data['ReZ'] = np.array(data_in.ReZ)
+    data['phase_rf'] = np.array(data_in.phase_rf)
+    data['HaX'] = np.array(data_in.HaX)
+    data['HaY'] = np.array(data_in.HaY)
+    data['HaZ'] = np.array(data_in.HaZ)
+    data['HeX'] = np.array(data_in.HeX)
+    data['HeY'] = np.array(data_in.HeY)
+    data['HeZ'] = np.array(data_in.HeZ)
+    data['mass'] = np.array(data_in.mass)
+
+    # Change phases to 0,1,2 encoding
+    data.loc[data.phase_lf==1, 'phase_lf'] = 0
+    data.loc[(data.phase_lf==2) | (data.phase_lf==3), 'phase_lf' ] = 1
+    data.loc[data.phase_lf==4, 'phase_lf'] = 2
     
-    total_column = ['LaX', 'LaY', 'LaZ', 'RaX', 'RaY', 'RaZ',
-                    'HaX', 'HaY', 'HaZ', 'LeX', 'LeY', 'LeZ',
-                    'ReX', 'ReY', 'ReZ', 'HeX', 'HeY', 'HeZ']
-    
-    X = data_pd[total_column].values
+    data.loc[data.phase_rf==2, 'phase_rf'] = 0
+    data.loc[(data.phase_rf==1) | (data.phase_rf==3), 'phase_rf'] = 1
+    data.loc[data.phase_rf==5, 'phase_rf'] = 2
+
+    # create dummy variables for phase
+    data.phase_lf = data.phase_lf.astype(long)
+    data.phase_rf = data.phase_rf.astype(long)
+
+    dum_phase_lf = pd.get_dummies(data.phase_lf, prefix = 'phase_lf').astype(long)
+    dum_phase_rf = pd.get_dummies(data.phase_rf, prefix = 'phase_rf').astype(long)
+    data = pd.concat([data, dum_phase_lf, dum_phase_rf], axis=1)
+
+    data.loc[data.phase_lf_0==0, 'phase_lf_0'] = -1
+    data.loc[data.phase_lf_1==0, 'phase_lf_1'] = -1
+    data.loc[data.phase_lf_2==0, 'phase_lf_2'] = -1
+    data.loc[data.phase_rf_0==0, 'phase_rf_0'] = -1
+    data.loc[data.phase_rf_1==0, 'phase_rf_1'] = -1
+    data.loc[data.phase_rf_2==0, 'phase_rf_2'] = -1
+
+    # Variable for change in euler angle
+    for sensor in ['L','H','R']:
+        for orientation in ['X', 'Y', 'Z']:
+            var1 = sensor+'e'+orientation
+            var2 = sensor+'a'+orientation
+            var1_d = var1+'_d'
+            var2_d = var2+'_d'
+            data[var1_d] = np.ediff1d(data[var1], to_begin=np.nan)
+            data.loc[np.abs(data[var1_d])>3.00, var1_d] = 0
+            data[var2_d] = np.ediff1d(data[var2], to_begin=np.nan)
+
+    # Add variables for total acceleration in all sensors and the changes
+    data['left_accel'] = np.sqrt(data.LaX**2+data.LaY**2+data.LaZ**2)
+    data['hip_accel'] = np.sqrt(data.HaX**2+data.HaY**2+data.HaZ**2)
+    data['right_accel'] = np.sqrt(data.RaX**2+data.RaY**2+data.RaZ**2)
+
+    for var in ['left_accel','hip_accel','right_accel']:
+        var_d = var+'_d'
+        data[var_d] = np.ediff1d(data[var], to_begin=np.nan)
+
+    # Define set of predictors to use
+    predictors = ['LaX', 'LaY', 'LaZ',
+                  'RaX', 'RaY', 'RaZ',
+                  'HaX', 'HaY', 'HaZ',
+                  'LeX_d', 'LeY_d', 'LeZ_d',
+                  'ReX_d', 'ReY_d', 'ReZ_d',
+                  'HeX_d', 'HeY_d', 'HeZ_d',
+                  'LaX_d', 'LaY_d', 'LaZ_d',
+                  'RaX_d', 'RaY_d', 'RaZ_d',
+                  'HaX_d', 'HaY_d', 'HaZ_d',
+                  'hip_accel','right_accel', 'left_accel',
+                  'left_accel_d', 'right_accel_d', 'hip_accel_d',
+                  'mass',
+                  'phase_lf_0','phase_lf_1','phase_lf_2',
+                  'phase_rf_0','phase_rf_1','phase_rf_2']
+
+    X = data[predictors].values
+
+    # check for missing data in any of the predictors and mark those rows
     missing_data = False
     nan_row = []
     if np.isnan(X).any():
@@ -59,73 +116,26 @@ def prepare_data(data, train=True):
     if missing_data:
         nan_row = np.unique(np.where(np.isnan(X))[0])
         X = np.delete(X, (nan_row), axis=0)
-    if train:
-        data_pd['total_load'] = np.array(data.LFz) + np.array(data.RFz)
-        Y = data_pd['total_load'].values
-        del data_pd  # not used in further computations
-        if missing_data:
-            Y = np.delete(Y, (nan_row), axis=0)
-        return X, Y
-    else:
-        return X, nan_row
-    
-def _model_fit(X, Y):
-    """Fits Gradient boosting regressor on the training data and returns the
-    fit object
 
-    Args:
-        X: matrix of predictors
-        Y: list of response variables
-    Returns:
-        Fitted model
-    """
-    params = {'n_estimators': 1000, 'max_depth': 4, 'min_samples_split': 1,
-              'learning_rate': 0.01, 'loss': 'lad'}
-    slr = GradientBoostingRegressor(**params)
-    fit = slr.fit(X, Y)
+    # pass the data through a low pass butterworth filter
+    # filtering has to be done after subsetting for nan
+    X1 = X[:,0:33]
+    X2 = X[:, 33:]
+    X1 = _filter_data(X1, )
+    X = np.append(X1,X2,1)
+    # scale the data
+    X = sc.transform(X)
+    return X, nan_row
 
-    return fit
 
-def train_model(data, path):
-    """Run the model training and model fit to pickle
-    Args:
-        data:
-        path: filepath where the fit object is pickled
-        
-    Returns:
-        None: pickles the fit object to the path provided
-    
-    """
-    
-    X, Y = prepare_data(data)
-    fit = _model_fit(X, Y)
-    
-    with open(path, 'wb') as file_path:
-        pickle.dump(fit, file_path)
-    
-if __name__ == '__main__':
-    pass
-#    import matplotlib.pyplot as plt
-#    import os
-#    import numpy as np
-#    import time
-##    os.chdir('C:\\Users\\dipesh\\Desktop\\biometrix\\python_scripts')
-#    from phaseDetection import combine_phase
-#    path = 'C:\\Users\\dipesh\\Desktop\\biometrix\\'
-#    data0 = np.genfromtxt(path+"combined\\sensor&grfdata.csv", delimiter=",",
-#                          names=True)
-#    sampl_rate = 250
-#    lf_phase, rf_phase = combine_phase(data0['LaZ'], data0['RaZ'], sampl_rate)
-#    data = pd.DataFrame(data0)
-#    data['phase_l'] = lf_phase
-#    data['phase_r'] = rf_phase
-#    s = time.time()
-#    train_model(data, path+"ms_trainmodel.pkl")
-#    print "it took", time.time()-s, "to train the model"
-#    with open(path + "fittedModel.pkl") as f:
-#        fit = pickle.load(f)
-#    X = prepareData(data,False)
-#    y_pred = fit.predict(X)
-#    y_true = (data['RFz']+data['LFz']).values
-#    diff = np.abs(y_true-y_pred)
-#    plt.plot(y_pred)
+def _filter_data(X, cutoff=6, fs=100, order=4):
+    """forward-backward lowpass butterworth filter
+    defaults:
+        cutoff freq: 12hz
+        sampling rage: 100hz
+        order: 4"""
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    X_filt = filtfilt(b, a, X, axis=0)
+    return X_filt
