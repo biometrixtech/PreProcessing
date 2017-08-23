7189a749d1cc273832c52fe1a535d3629639a718
Stephen Poole <stephen@melon.software>
2017-08-23 09:30:22 +0100

    Typo

--- app/src/Version_2.2/batch_entrypoint.py
+++ app/src/Version_2.2/batch_entrypoint.py
@@ -106,7 +106,7 @@ if __name__ == '__main__':
 
             from downloadAndChunk import downloadAndChunk
             s3_bucket = input_data.get('S3Bucket', None)
-            s3_basepath = input_data.get('S3Basepath', None)
+            s3_basepath = input_data.get('S3BasePath', None)
             s3_paths = [s3_basepath + suffix for suffix in input_data.get('S3Suffixes', [])]
             tmp_filename = downloadAndChunk.script_handler(s3_bucket, s3_paths)
 
