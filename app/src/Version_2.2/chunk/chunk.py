a455637a62ebb08b22c03fa4426cf45752729d5f
Stephen Poole <stephen@melon.software>
2017-09-23 14:47:11 +0100

    Try and clean up chunking code

--- app/src/Version_2.2/chunk/chunk.py
+++ app/src/Version_2.2/chunk/chunk.py
@@ -11,7 +11,7 @@ logger.setLevel(logging.INFO)
 
 def chunk_by_line(input_filename, output_dir, chunk_size):
     # Get the column headers (first line of first file)
-    header_filename = '{base_fn}_header'.format(base_fn=input_filename)
+    header_filename = os.path.join('/tmp', input_filename + '_header')
     os.system(
         'head -n 1 {tmp_filename} > {header_filename}'.format(
             tmp_filename=input_filename,
@@ -20,34 +20,45 @@ def chunk_by_line(input_filename, output_dir, chunk_size):
     )
 
     # Strip the header from the file
-    body_filename = input_filename + '_body'
-    os.system('tail -n +2 {tmp_filename} > {body_filename}'.format(tmp_filename=input_filename, body_filename=body_filename))
+    body_filename = os.path.join('/tmp', input_filename + '_body')
+    os.system(
+        'tail -n +2 {input_filename} > {body_filename}'.format(
+            input_filename=input_filename,
+            body_filename=body_filename
+        )
+    )
 
     # Divide file into chunks
+    tmp_chunk_dir = os.path.join('/tmp', input_filename)
     if isinstance(chunk_size, list):
-        subprocess.call(['csplit', '-f', input_filename, '-b', '_%02d', body_filename] + [str(l) for l in chunk_size])
+        subprocess.call(['csplit', '-f', tmp_chunk_dir, '-b', '_%02d', body_filename] + [str(l) for l in chunk_size])
     else:
         subprocess.call([
             'split',
             '-l', str(chunk_size),
             '-d',
             body_filename,
-            input_filename + '_',
+            tmp_chunk_dir + '_',
         ])
 
-    # Prepend the column headers to each file and copy to the EFS directory
+    # Prepend the column headers to each file and copy to the output directory
     file_names = []
-    for file in glob.glob(input_filename + '_[0-9]*'):
+    for file in glob.glob(tmp_chunk_dir + '_[0-9]*'):
         file_name = os.path.basename(file)
-
-        with open(output_dir + '/' + file_name, 'w') as efs_output:
-            subprocess.call(['cat', header_filename, file], stdout=efs_output)
+        output_filepath = os.path.join(output_dir, file_name)
+        os.system(
+            'cat {header_filename} {file} > {output_filepath}'.format(
+                header_filename=header_filename,
+                file=file,
+                output_filepath=output_filepath
+            )
+        )
 
         # Clean up /tmp directory
         os.remove(file)
 
         logger.info('Processed "{}" chunk'.format(file))
-        file_names.append(file_name)
+        file_names.append(output_filepath)
 
     os.remove(body_filename)
     os.remove(header_filename)
