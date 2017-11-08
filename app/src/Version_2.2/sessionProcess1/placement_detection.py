d89e2194b4742cbca8d827458056437f7825f279
DIpesh Gautam <dipesh@biometrixtech.com>
2017-11-08 14:27:50 -0500

    Update to data subsetting for activity

--- app/src/Version_2.2/sessionProcess1/placement_detection.py
+++ app/src/Version_2.2/sessionProcess1/placement_detection.py
@@ -8,11 +8,14 @@ import quatConvs as qc
 def detect_placement(data):
     """Detect placement of sensors using accleration and pitch
     """
+    data.reset_index(inplace=True)
     data = shift_accel(data)
-    walking = detect_activity(data)
-    data = data.loc[walking[0]:walking[1], :]
+    start, end = detect_activity(data)
+    data = data.loc[start[0]:end[0], :]
     data.reset_index(inplace=True)
     hip = hips_vs_feet(data)
+
+    # based on the hip detected, assign other two sensors as foot1 and foot2
     if hip == 0:
         qW1 = data.qW1.values.reshape(-1, 1)
         qX1 = data.qX1.values.reshape(-1, 1)
@@ -33,7 +36,7 @@ def detect_placement(data):
         pitch_foot1 = euls_1[:, 1]
         pitch_foot2 = euls_2[:, 1]
 
-        right_0, left_0 = left_vs_right(pitch_foot1, pitch_foot2)
+        left_0, right_0 = left_vs_right(pitch_foot1, pitch_foot2)
         right = right_0
         left = left_0
 
@@ -57,7 +60,7 @@ def detect_placement(data):
         pitch_foot1 = euls_1[:, 1]
         pitch_foot2 = euls_2[:, 1]
 
-        right_0, left_0 = left_vs_right(pitch_foot1, pitch_foot2)
+        left_0, right_0 = left_vs_right(pitch_foot1, pitch_foot2)
         if right_0 == 1:
             right = 0
             left = 2
@@ -85,7 +88,7 @@ def detect_placement(data):
         pitch_foot1 = euls_1[:, 1]
         pitch_foot2 = euls_2[:, 1]
 
-        right_0, left_0 = left_vs_right(pitch_foot1, pitch_foot2)
+        left_0, right_0 = left_vs_right(pitch_foot1, pitch_foot2)
         if right_0 == 1:
             right = 0
             left = 1
@@ -101,7 +104,6 @@ def detect_activity(data):
     """
     thresh = 5.  # threshold to detect balance phase
     bal_win = 100 # sampling window to determine balance phase
-    min_mov = 100
     acc_mag_0 = np.sqrt(data.aX0**2 + data.aY0**2 + data.aZ0**2)
     acc_mag_1 = np.sqrt(data.aX1**2 + data.aY1**2 + data.aZ1**2)
     acc_mag_2 = np.sqrt(data.aX2**2 + data.aY2**2 + data.aZ2**2)
@@ -126,10 +128,26 @@ def detect_activity(data):
     start_bal = start_bal.tolist()  # convert from numpy array to list
     # delete variables that are of no use in further compuations
     del dummy_balphase
-    change = np.where(np.ediff1d(start_bal) > min_mov)[0]
+    min_thresh_mov = 300 # threshold for min number of samples 
+                        # required to be classified as false movement phase
+    for i in range(len(start_bal) - 1):
+        diff = start_bal[i+1] - start_bal[i]
+        if 1 < diff <= min_thresh_mov:
+            for j in range(1, diff+1):
+                start_bal.append(start_bal[i]+j)
+    mov = np.ones(len(data))
+    mov[start_bal] = 0
+    change = np.ediff1d(mov, to_begin=0)
+    start = np.where(change==1)[0]
+    end = np.where(change==-1)[0]
 
-    start = start_bal[change[0]] - 200
-    end = start + 1700
+    # if data ends with movement, assign final point as end of movement
+    if len(start) != len(end):
+        end = np.append(end, len(data))
+
+    start = start - 100
+    for i in range(len(end)):
+        end[i] = min([end[i], start[i] + 1100])
 
     return start, end
 
@@ -138,7 +156,7 @@ def shift_accel(data):
     """Adjust acceleration so that all axes are centered around 0
     """
     data.aX0 = data.aX0 - np.nanmean(data.aX0)
-    data.aX0 = data.aX0 - np.nanmean(data.aX0)
+    data.aY0 = data.aY0 - np.nanmean(data.aY0)
     data.aZ0 = data.aZ0 - np.nanmean(data.aZ0)
 
     data.aX1 = data.aX1 - np.nanmean(data.aX1)
@@ -155,15 +173,14 @@ def shift_accel(data):
 def hips_vs_feet(data):
     """use sum of square of acceleration during movement to detect hips from feet
     """
-    sum_sq_0 = np.sum(data.aX0**2 + data.aY0**2 + data.aZ0**2) / 1000
-    sum_sq_1 = np.sum(data.aX1**2 + data.aY1**2 + data.aZ1**2) / 1000
-    sum_sq_2 = np.sum(data.aX2**2 + data.aY2**2 + data.aZ2**2) / 1000
+    sum_sq_0 = np.nansum(data.aX0**2 + data.aY0**2 + data.aZ0**2) / 1000
+    sum_sq_1 = np.nansum(data.aX1**2 + data.aY1**2 + data.aZ1**2) / 1000
+    sum_sq_2 = np.nansum(data.aX2**2 + data.aY2**2 + data.aZ2**2) / 1000
 
     ratio = [0, 0, 0]
     ratio[0] = np.min([sum_sq_1, sum_sq_2]) / sum_sq_0
     ratio[1] = np.min([sum_sq_0, sum_sq_2]) / sum_sq_1
     ratio[2] = np.min([sum_sq_0, sum_sq_1]) / sum_sq_2
-    print(ratio)
 
     if np.max(ratio) == ratio[0] and ratio[0] >= 1.5:
         hip = 0
@@ -172,7 +189,7 @@ def hips_vs_feet(data):
     elif np.max(ratio) == ratio[2] and ratio[2] >= 1.5:
         hip = 2
     else:
-        raise(ValueError)
+        raise(ValueError) # placeholder for inability to detect hips vs feet
 
     return hip
 
@@ -180,16 +197,19 @@ def hips_vs_feet(data):
 def left_vs_right(pitch_foot1, pitch_foot2):
     """Use raw pitch value and the direction of change to detect left vs right foot
     """
+    # TODO: placeholder logic for now, needs update
     mean1 = np.nanmean(pitch_foot1[0:100])
     mean2 = np.nanmean(pitch_foot2[0:100])
 
-    diff1 = np.nansum(pitch_foot1 - mean1)
-    diff2 = np.nansum(pitch_foot2 - mean2)
+    diff1 = np.nansum(pitch_foot1[100:] - mean1)
+    diff2 = np.nansum(pitch_foot2[100:] - mean2)
 
     if diff2 > diff1:
         right = 2
         left = 1
-    else:
+    elif diff1 > diff2:
         right = 1
         left = 2
+    else:
+        raise(ValueError) #placeholder for inability to detect right vs left
     return left, right
