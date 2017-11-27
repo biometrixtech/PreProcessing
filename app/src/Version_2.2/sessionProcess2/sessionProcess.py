ffa77fa8bb72c66e6c0680253ea4c8401d38cc3b
courtney-white <courtney@biometrixtech.com>
2017-11-27 11:27:24 -0500

    Update sessionProcess.py
    Update of CFT method to match Daniele's c code results

--- app/src/Version_2.2/sessionProcess2/sessionProcess.py
+++ app/src/Version_2.2/sessionProcess2/sessionProcess.py
@@ -64,33 +64,59 @@ def make_quaternion_array(quaternion, length):
     return np.array([quaternion for i in range(length)])
 
 
+
 def apply_data_transformations(sdata, bf_transforms, hip_neutral_transform):
+    '''
+    Use the body frame transforms and the hip neutral transform calculated
+    during calibration to convert the data in the raw sensor frame to
+    coordinate frames which are useful, namely, the adjusted inertial frame
+    for acceleration and the body frame for orientation.
+
+    Args:
+        sdata - data frame
+        bf_transforms - 3 (4x1) transform values (left, hip, and right)
+            calculated from data of the user standing still during calibration,
+            which can be used to convert practice data to the body frame
+        hip_neutral_transform - (4x1) transform value which includes the yaw
+            misalignment of the hip in a neutral position, as recorded from a
+            user standing still during calibration, with a correction of 90
+            degrees to correct for known additional offset of the hip sensor
+            frame from the body frame
+
+    Returns:
+        sdata - data frame with acceleration and orientation data overwritten
+        to be in the correct coordinate frames
+
+    '''
+
     # Number of records
     row_count = sdata.shape[0]
 
-    # Create arrays of the normalisation quaternions
+    # Create arrays of the transformation quaternions
     q_bftransform_left = make_quaternion_array(bf_transforms['Left'], row_count)
     q_bftransform_hip = make_quaternion_array(bf_transforms['Hip'], row_count)
     q_bftransform_right = make_quaternion_array(bf_transforms['Right'], row_count)
-    q_neutraltransform_hip = make_quaternion_array(hip_neutral_transform, row_count)
+    q_neutraltransform_hip = make_quaternion_array(hip_neutral_transform['hip'], row_count)
 
-    # Extract the position quaternions from the frames
+    # Extract the orientation quaternions from the data
     q_sensor_left = sdata.loc[:, ['LqW', 'LqX', 'LqY', 'LqZ']].values.reshape(-1, 4)
     q_sensor_hip = sdata.loc[:, ['HqW', 'HqX', 'HqY', 'HqZ']].values.reshape(-1, 4)
     q_sensor_right = sdata.loc[:, ['RqW', 'RqX', 'RqY', 'RqZ']].values.reshape(-1, 4)
 
-    # Apply body frame transform to normalise pitch and roll
+    # Apply body frame transform to transform pitch and roll in feet
     q_bf_left = quat_prod(q_sensor_left, q_bftransform_left)
-    q_bf_hip = quat_prod(q_sensor_hip, q_bftransform_hip)
     q_bf_right = quat_prod(q_sensor_right, q_bftransform_right)
 
     # Rotate left foot by 180º
     yaw_180 = make_quaternion_array([0, 0, 0, 1], row_count)
     q_bf_left = quat_prod(q_bf_left, yaw_180)
 
-    # Rotate hip sensor by 90º plus the hip neutral transform
-    yaw_90 = make_quaternion_array([sqrt(2)/2, 0, 0, -sqrt(2)/2], row_count)
-    q_bf_hip = quat_multi_prod(q_bf_hip, yaw_90, q_neutraltransform_hip)
+    # Rotate hip sensor by 90º plus the hip neutral transform, find the body
+    # frame of the hip data
+    yaw_90 = make_quaternion_array([sqrt(2)/2, 0, 0, -sqrt(2)/2], row_count)    
+    q_bf_hip = quat_prod(q_bftransform_hip, yaw_90)
+    q_bf_hip = quat_prod(q_sensor_hip, q_bf_hip)
+    q_bf_hip = quat_prod(q_neutraltransform_hip, q_bf_hip)
 
     # Isolate the yaw component of the instantaneous sensor orientations
     q_bf_yaw_left = quat_force_euler_angle(q_bf_left, phi=0, theta=0)
@@ -98,18 +124,18 @@ def apply_data_transformations(sdata, bf_transforms, hip_neutral_transform):
     q_bf_yaw_right = quat_force_euler_angle(q_bf_right, phi=0, theta=0)
 
     # Extract the sensor-frame acceleration values and create imaginary quaternions
-    acc_sensor_left = np.hstack([sdata.LaX, sdata.LaY, sdata.LaZ]).reshape(-1, 3)
-    acc_sensor_hip = np.hstack([sdata.HaX, sdata.HaY, sdata.HaZ]).reshape(-1, 3)
-    acc_sensor_right = np.hstack([sdata.RaX, sdata.RaY, sdata.RaZ]).reshape(-1, 3)
-    print('acc_sensor_left = {}'.format(acc_sensor_left[1,:] * 1000 / 9.80655))
-    print('acc_sensor_hip = {}'.format(acc_sensor_hip[1,:] * 1000 / 9.80655))
-    print('acc_sensor_right = {}'.format(acc_sensor_right[1,:] * 1000 / 9.80655))
+    acc_sensor_left = sdata.loc[:, ['LaX', 'LaY', 'LaZ']].values.reshape(-1, 3)
+    acc_sensor_hip = sdata.loc[:, ['HaX', 'HaY', 'HaZ']].values.reshape(-1, 3)
+    acc_sensor_right = sdata.loc[:, ['RaX', 'RaY', 'RaZ']].values.reshape(-1, 3)
+#    print('acc_sensor_left = {}'.format(acc_sensor_left[1,:] * 1000 / 9.80655))
+#    print('acc_sensor_hip = {}'.format(acc_sensor_hip[1,:] * 1000 / 9.80655))
+#    print('acc_sensor_right = {}'.format(acc_sensor_right[1,:] * 1000 / 9.80655))
 
     # Transform left sensor
     acc_aiftransform_left = quat_prod(quat_conj(q_bf_yaw_left), q_sensor_left)
-    print('acc_aiftransform_left = {}'.format(acc_aiftransform_left[1,:]))
-    acc_aif_left = vect_rot(acc_sensor_left, quat_conj(acc_aiftransform_left))
-    print('acc_aif_left = {}'.format(acc_aif_left[1,:] * 1000 / 9.80655))
+#    print('acc_aiftransform_left = {}'.format(acc_aiftransform_left[1,:]))
+    acc_aif_left = vect_rot(acc_sensor_left, acc_aiftransform_left)
+#    print('acc_aif_left = {}'.format(acc_aif_left[1,:] * 1000 / 9.80655))
 
     # Apply hip transformation
     acc_aiftransform_hip = quat_multi_prod(
@@ -117,15 +143,15 @@ def apply_data_transformations(sdata, bf_transforms, hip_neutral_transform):
         q_neutraltransform_hip,
         q_sensor_hip,
     )
-    print('acc_aiftransform_left = {}'.format(acc_aiftransform_left[1,:]))
+#    print('acc_aiftransform_hip = {}'.format(acc_aiftransform_hip[1,:]))
     acc_aif_hip = vect_rot(acc_sensor_hip, quat_conj(acc_aiftransform_hip))
-    print('acc_aif_hip = {}'.format(acc_aif_hip[1,:] * 1000 / 9.80655))
+#    print('acc_aif_hip = {}'.format(acc_aif_hip[1,:] * 1000 / 9.80655))
 
     # Transform right sensor
-    acc_aiftransform_right = quat_prod(quat_conj(q_bf_yaw_right), q_sensor_right)
-    print('acc_aiftransform_left = {}'.format(acc_aiftransform_left[1,:]))
-    acc_aif_right = vect_rot(acc_sensor_right, quat_conj(acc_aiftransform_right))
-    print('acc_aif_right = {}'.format(acc_aif_right[1,:] * 1000 / 9.80655))
+    acc_aiftransform_right = quat_prod(quat_conj(q_sensor_right), q_bf_yaw_right)
+#    print('acc_aiftransform_right = {}'.format(acc_aiftransform_right[1,:]))
+    acc_aif_right = vect_rot(acc_sensor_right, acc_aiftransform_right)
+#    print('acc_aif_right = {}'.format(acc_aif_right[1,:] * 1000 / 9.80655))
 
     # Re-insert the updated values
     sdata.loc[:, ['LqW', 'LqX', 'LqY', 'LqZ']] = q_bf_left
@@ -135,12 +161,17 @@ def apply_data_transformations(sdata, bf_transforms, hip_neutral_transform):
     sdata.loc[:, ['HaX', 'HaY', 'HaZ']] = acc_aif_hip
     sdata.loc[:, ['RaX', 'RaY', 'RaZ']] = acc_aif_right
 
+    # subtract the effects of gravity
+    sdata = apply_acceleration_normalisation(sdata)
+
     return sdata
 
 
 def apply_acceleration_normalisation(sdata):
     # Remove the effects of gravity
+    sdata.LaZ -= 9.80665
     sdata.HaZ -= 9.80665
+    sdata.RaZ -= 9.80665
     return sdata
 
 
