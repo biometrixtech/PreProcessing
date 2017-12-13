3435199e1640567a31cff84b190f52881b15bb93
DIpesh Gautam <dipesh@biometrixtech.com>
2017-12-12 20:47:23 -0500

    debug

--- app/sessionAgg/agg_session.py
+++ app/sessionAgg/agg_session.py
@@ -160,7 +160,9 @@ def script_handler(working_directory, input_data):
 
         # fatigue analysis
         session_fatigue = _fatigue_analysis(data, var='perc_optimal')
-        if session_fatigue is numpy.nan:
+        print(session_fatigue)
+        if session_fatigue == numpy.nan:
+            print('session fatigue nan')
             session_fatigue = 0
 
         # create ordered dictionary object
