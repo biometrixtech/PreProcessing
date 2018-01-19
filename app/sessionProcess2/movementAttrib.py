9c3a55acd12b0be20593ce354bd67728f118e8b7
courtney-white <courtney@biometrixtech.com>
2018-01-19 17:40:45 -0500

    Update movementAttrib.py
    removed some corner case handling of close-to-overlapping double leg activity, and some smoothing, which would likely eliminate some of the detail we would want

--- app/sessionProcess2/movementAttrib.py
+++ app/sessionProcess2/movementAttrib.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Dec 05 14:40:57 2017
+Created on Tue Dec 05 14:40:57 201
 
 @author: court
 """
@@ -31,7 +31,7 @@ def plane_analysis(hip_acc, hip_eul, ms_elapsed):
             stationary_binary,
             accel_mag
     """
-    
+
     # create storage for variables
     len_hip_acc = len(hip_acc)
     sampling_rate = 1000/ms_elapsed
@@ -99,7 +99,7 @@ def plane_analysis(hip_acc, hip_eul, ms_elapsed):
     rot_binary[(np.abs(rot)>0.2) & (stationary_binary==0)] = 1
     
     return lat, vert, horz, rot, lat_binary, vert_binary, horz_binary,\
-            rot_binary, stationary_binary, accel_mag.reshape(-1, 1)
+           rot_binary, stationary_binary, accel_mag.reshape(-1, 1)
 
 
 def run_stance_analysis(data):
@@ -169,6 +169,7 @@ def run_stance_analysis(data):
         left_impact, right_impact, double_impact, left_takeoff, right_takeoff,\
         double_takeoff, feet_eliminated = sort_phases(lf_ph, rf_ph, not_standing, hz, hip_tot_acc)
 
+
     # enumerate stance
     stance = enumerate_stance(left_dyn_balance, right_dyn_balance, double_dyn_balance, \
         left_stat_balance, right_stat_balance, double_stat_balance, \
@@ -182,7 +183,7 @@ def standing_or_not(hip_eul, hz):
     """Determine when the subject is standing or not.
     
     Args:
-        hip_eul: body frame euler angle position data at hip
+        hip_eul: body frame euler angle position data at hip        
         hz: an int, sampling rate of sensor
         
     Returns:
@@ -268,7 +269,7 @@ def sort_phases(lf_ph, rf_ph, not_standing, hz, hacc):
         left_dyn_balance: binary column identifying when user is balancing on
             their left leg while in motion
         right_dyn_balance: binary column identifying when user is balancing on
-            their right leg while in motion
+            their right leg while in motio
         double_dyn_balance: binary column identifying when user is standing on
             both legs while moving (at the hips)
         left_stat_balance: binary column identifying when user is standing
@@ -307,7 +308,6 @@ def sort_phases(lf_ph, rf_ph, not_standing, hz, hacc):
     left_impact = np.zeros((len(lf_ph), 1))
     right_impact = np.zeros((len(lf_ph), 1))
     double_impact = np.zeros((len(lf_ph), 1))
-
     left_takeoff = np.zeros((len(lf_ph), 1))
     right_takeoff = np.zeros((len(lf_ph), 1))
     double_takeoff = np.zeros((len(lf_ph), 1))
@@ -328,49 +328,49 @@ def sort_phases(lf_ph, rf_ph, not_standing, hz, hacc):
 
 
     # smooth dynamic balance where gaps of less than 3 ind show phase changes
-    zero_indices = _num_runs(left_dyn_balance, 0)
-    diff_0s = zero_indices[:, 1] - zero_indices[:, 0]
-
-    sig_diff_0s = zero_indices[np.where(diff_0s<3), :]
-    del zero_indices, diff_0s  # not used in further computations
-    sig_diff_0s = sig_diff_0s.reshape(len(sig_diff_0s[0]), 2)
-
-    for i in range(len(sig_diff_0s)):
-        left_dyn_balance[sig_diff_0s[i][0]:sig_diff_0s[i][1]] = 1
-    del sig_diff_0s # not used in further computations
-    
-    zero_indices = _num_runs(right_dyn_balance, 0)
-    diff_0s = zero_indices[:, 1] - zero_indices[:, 0]
-
-    sig_diff_0s = zero_indices[np.where(diff_0s<3), :]
-    del zero_indices, diff_0s  # not used in further computations
-    sig_diff_0s = sig_diff_0s.reshape(len(sig_diff_0s[0]), 2)
-
-    for i in range(len(sig_diff_0s)):
-        right_dyn_balance[sig_diff_0s[i][0]:sig_diff_0s[i][1]] = 1
-    del sig_diff_0s # not used in further computations
-    
-    one_indices = _num_runs(left_dyn_balance, 1)
-    diff_1s = one_indices[:, 1] - one_indices[:, 0]
-
-    sig_diff_1s = one_indices[np.where(diff_1s<3), :]
-    del one_indices, diff_1s  # not used in further computations
-    sig_diff_1s = sig_diff_1s.reshape(len(sig_diff_1s[0]), 2)
-
-    for i in range(len(sig_diff_1s)):
-        left_dyn_balance[sig_diff_1s[i][0]:sig_diff_1s[i][1]] = 1
-    del sig_diff_1s # not used in further computations
-    
-    one_indices = _num_runs(right_dyn_balance, 1)
-    diff_1s = one_indices[:, 1] - one_indices[:, 0]
-
-    sig_diff_1s = one_indices[np.where(diff_1s<3), :]
-    del one_indices, diff_1s  # not used in further computations
-    sig_diff_1s = sig_diff_1s.reshape(len(sig_diff_1s[0]), 2)
-
-    for i in range(len(sig_diff_1s)):
-        right_dyn_balance[sig_diff_1s[i][0]:sig_diff_1s[i][1]] = 1
-    del sig_diff_1s # not used in further computations
+#    zero_indices = _num_runs(left_dyn_balance, 0)
+#    diff_0s = zero_indices[:, 1] - zero_indices[:, 0]
+#
+#    sig_diff_0s = zero_indices[np.where(diff_0s<3), :]
+#    del zero_indices, diff_0s  # not used in further computations
+#    sig_diff_0s = sig_diff_0s.reshape(len(sig_diff_0s[0]), 2)
+#
+#    for i in range(len(sig_diff_0s)):
+#        left_dyn_balance[sig_diff_0s[i][0]:sig_diff_0s[i][1]] = 1
+#    del sig_diff_0s # not used in further computations
+#    
+#    zero_indices = _num_runs(right_dyn_balance, 0)
+#    diff_0s = zero_indices[:, 1] - zero_indices[:, 0]
+#
+#    sig_diff_0s = zero_indices[np.where(diff_0s<3), :]
+#    del zero_indices, diff_0s  # not used in further computations
+#    sig_diff_0s = sig_diff_0s.reshape(len(sig_diff_0s[0]), 2)
+#
+#    for i in range(len(sig_diff_0s)):
+#        right_dyn_balance[sig_diff_0s[i][0]:sig_diff_0s[i][1]] = 1
+#    del sig_diff_0s # not used in further computations
+#    
+#    one_indices = _num_runs(left_dyn_balance, 1)
+#    diff_1s = one_indices[:, 1] - one_indices[:, 0]
+#
+#    sig_diff_1s = one_indices[np.where(diff_1s<3), :]
+#    del one_indices, diff_1s  # not used in further computations
+#    sig_diff_1s = sig_diff_1s.reshape(len(sig_diff_1s[0]), 2)
+#
+#    for i in range(len(sig_diff_1s)):
+#        left_dyn_balance[sig_diff_1s[i][0]:sig_diff_1s[i][1]] = 1
+#    del sig_diff_1s # not used in further computations
+# 
+#    one_indices = _num_runs(right_dyn_balance, 1)
+#    diff_1s = one_indices[:, 1] - one_indices[:, 0]
+#
+#    sig_diff_1s = one_indices[np.where(diff_1s<3), :]
+#    del one_indices, diff_1s  # not used in further computations
+#    sig_diff_1s = sig_diff_1s.reshape(len(sig_diff_1s[0]), 2)
+#
+#    for i in range(len(sig_diff_1s)):        
+#        right_dyn_balance[sig_diff_1s[i][0]:sig_diff_1s[i][1]] = 1
+#    del sig_diff_1s # not used in further computations
 
 
     # differentiate between static and dynamic balance phases
@@ -407,9 +407,8 @@ def sort_phases(lf_ph, rf_ph, not_standing, hz, hacc):
     left_dyn_balance[left_stat_balance == 1] = 0
     right_dyn_balance[right_stat_balance == 1] = 0
 
-
     # determine where double leg stance applicable
-    
+
     # where single leg things overlap, assign double leg
     double_dyn_balance[(lf_ph == 1) & (rf_ph == 2)] = 1
     double_dyn_balance[(lf_ph == 0) | (rf_ph == 0)] = 1
@@ -432,37 +431,37 @@ def sort_phases(lf_ph, rf_ph, not_standing, hz, hacc):
     # where single leg things start very close to each other, assign double leg
     doub_range = 3 # radius for features to begin in for left + right to = doub
 
-    r_one_indices = _num_runs(right_dyn_balance, 1)
-    l_one_indices = _num_runs(left_dyn_balance, 1)
-    all_l_one_indices = np.array([])
-
-    for k in range(int(l_one_indices.shape[0])):
-
-        all_l_one_indices = np.hstack((all_l_one_indices,
-                                       np.asarray(range(l_one_indices[k, 0] 
-                                       - doub_range, l_one_indices[k, 0]
-                                       + doub_range))))
-
-    set_all_l_one_indices = set(all_l_one_indices)
-
-    for k in range(int(r_one_indices.shape[0])):
-        r_run = range(r_one_indices[k, 0] - doub_range, r_one_indices[k, 0]
-                      + doub_range)
-        r_win = set(r_run)
-        intersect = set_all_l_one_indices.intersection(r_win)
-        if len(intersect) != 0: # there are close impacts
-            # assign full value of windows containing that index to be doub leg
-            double_dyn_balance[range(r_one_indices[k, 0],
-                                     r_one_indices[k, 1])] = 1
-            for r in range(int(l_one_indices.shape[0])):
-                l_run = range(l_one_indices[r, 0], l_one_indices[r, 1])
-                l_win = set(l_run)
-                intersect2 = r_win.intersection(l_win)
-                if len(intersect2) != 0:
-                    double_dyn_balance[l_run] = 1
-                del l_run, l_win
-        del r_run, r_win
-    del r_one_indices, l_one_indices, all_l_one_indices, set_all_l_one_indices
+#    r_one_indices = _num_runs(right_dyn_balance, 1)
+#    l_one_indices = _num_runs(left_dyn_balance, 1)
+#    all_l_one_indices = np.array([])
+#
+#    for k in range(int(l_one_indices.shape[0])):
+#
+#        all_l_one_indices = np.hstack((all_l_one_indices,
+#                                       np.asarray(range(l_one_indices[k, 0] 
+#                                       - doub_range, l_one_indices[k, 0]
+#                                       + doub_range))))
+#
+#    set_all_l_one_indices = set(all_l_one_indices)
+#
+#    for k in range(int(r_one_indices.shape[0])):
+#        r_run = range(r_one_indices[k, 0] - doub_range, r_one_indices[k, 0]
+#                      + doub_range)
+#        r_win = set(r_run)
+#        intersect = set_all_l_one_indices.intersection(r_win)
+#        if len(intersect) != 0: # there are close impacts
+#            # assign full value of windows containing that index to be doub leg
+#            double_dyn_balance[range(r_one_indices[k, 0],
+#                                     r_one_indices[k, 1])] = 1
+#            for r in range(int(l_one_indices.shape[0])):
+#                l_run = range(l_one_indices[r, 0], l_one_indices[r, 1])
+#                l_win = set(l_run)
+#                intersect2 = r_win.intersection(l_win)
+#                if len(intersect2) != 0:
+#                    double_dyn_balance[l_run] = 1
+#                del l_run, l_win
+#        del r_run, r_win
+#    del r_one_indices, l_one_indices, all_l_one_indices, set_all_l_one_indices
 
     r_one_indices = _num_runs(right_impact, 1)
 
@@ -540,7 +539,7 @@ def sort_phases(lf_ph, rf_ph, not_standing, hz, hacc):
     left_takeoff[not_standing == 1] = 0
     right_takeoff[not_standing == 1] = 0
     double_takeoff[not_standing == 1] = 0
-    
+
     # remove data where neither foot is on ground
     left_dyn_balance[lf_ph == 3] = 0
     right_dyn_balance[lf_ph == 3] = 0
@@ -692,30 +691,30 @@ def enumerate_stance(left_dyn_balance, right_dyn_balance, double_dyn_balance, \
     stance[right_takeoff == 1] = 8
     stance[double_takeoff == 1] = 9
     stance[feet_eliminated == 1] = 1
-    
+
     return stance
-    
 
 
-#%% Run the data
-if __name__ == '__main__':
-    import pandas as pd
 
-    '''
-    run this on data which has been transformed and which has proper phase detection
-    
-    code flow:
-        run_stance_analysis -- 
-        pulls data from frame
-        finds total acceleration (identical to v1 version)
-        determines if data is from user standing or not (identical to v1 method)
-        sorts data into stance subcategories by phase
-        enumerates stance        
-    '''
-
-    data = 'phase_transformed_f03d706b-75cc-49db-a4ae-ca1eecca789f'
-    data = data + '.csv'
-
-    sdata = pd.read_csv(data)
-
-    stance = run_stance_analysis(sdata)
+##%% Run the data
+#if __name__ == '__main__':
+#    import pandas as pd
+#
+#    '''
+#    run this on data which has been transformed and which has proper phase detection
+#    
+#    code flow:
+#        run_stance_analysis -- 
+#        pulls data from frame
+#        finds total acceleration (identical to v1 version)
+#        determines if data is from user standing or not (identical to v1 method)
+#        sorts data into stance subcategories by phase
+#        enumerates stance        
+#    '''
+#
+#    data = 'phase_transformed_f03d706b-75cc-49db-a4ae-ca1eecca789f'
+#    data = data + '.csv'
+#
+#    sdata = pd.read_csv(data)
+#
+#    stance = run_stance_analysis(sdata)
