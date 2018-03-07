9214c47ed6b6e2b25eae0e35d403311550bb29e9
Stephen Poole <stephen@melon.software>
2018-03-07 18:28:19 +0000

    Add put data

--- app/filestore.py
+++ app/filestore.py
@@ -1,8 +1,8 @@
 from io import StringIO
 from shutil import copyfile
-import os
-
+import errno
 import glob
+import os
 import pandas
 
 
@@ -18,31 +18,69 @@ class Filestore:
         'scoring': ('combined', 'csv'),
     }
 
-    def get_data(self, from_stage):
+    def get_data(self, from_stage, part_number=None):
         if from_stage not in self.stages:
             raise ValueError('Unknown stage {}'.format(from_stage))
         plurality, encoding = self.stages[from_stage]
 
-        if plurality == 'chunked':
+        if plurality == 'chunked' and part_number is None:
             data = self._read_multiple_csv(from_stage)
         else:
-            data = self._read_single_csv(from_stage)
+            data = self._read_single_csv(from_stage, part_number)
 
         if encoding == 'binary':
-            raise NotImplementedError
+            # TODO
+            raise NotImplementedError()
 
         return data
 
-    def _read_single_csv(self, stage):
+    def put_data(self, as_stage, data, columns, part_number=None):
+        if as_stage not in self.stages:
+            raise ValueError('Unknown stage {}'.format(as_stage))
+        plurality, encoding = self.stages[as_stage]
+
+        # First save the file to a temporary location
+        if encoding == 'binary':
+            # In this case, the `data` is actually the temporary filename
+            tmp_filename = data
+        else:
+            # Data is a pandas DataFrame object
+            tmp_filename = '/tmp/output'
+            tmp_file = open('/tmp/output', 'wb')
+            data.to_csv(tmp_file, index=False, na_rep='', columns=columns)
+
+        # Now move the file into the correct location
+        if plurality == 'chunked':
+            if part_number is None:
+                raise ValueError('Output of {} is chunked, must supply a part number'.format(as_stage))
+
+            self._mkdir(os.path.join(self.working_directory, as_stage))
+            if part_number == '*':
+                # This is the whole file, we need to chunk it
+                # TODO
+                raise NotImplementedError()
+            else:
+                os.rename(tmp_filename, os.path.join(self.working_directory, as_stage, part_number))
+
+        else:
+            self._mkdir(self.working_directory)
+            os.rename(tmp_filename, os.path.join(self.working_directory, as_stage))
+
+    def _read_single_csv(self, stage, part_number=None):
         """
         Read a single CSV file with pandas.  Copy the file to the local filesystem first,
         as that improves read performance.
         :param stage:
         :return:
         """
+        if part_number is not None:
+            source_filename = os.path.join(self.working_directory, stage, part_number)
+        else:
+            source_filename = os.path.join(self.working_directory, stage)
+
         tmp_filename = '/tmp/readfile'
-        copyfile(os.path.join(self.working_directory, stage), tmp_filename)
-        self._logger.info("Copied data file to local FS")
+        copyfile(source_filename, tmp_filename)
+        self._logger.info("Copied {} to local FS".format(source_filename))
         data = pandas.read_csv(tmp_filename)
         os.remove(tmp_filename)
         self._logger.info("Removed temporary file")
@@ -76,6 +114,18 @@ class Filestore:
         self._logger.info("{} rows".format(len(csv_data) - 1))
         csv_data = u"\n".join(csv_data)
         return StringIO(csv_data)
+
+    @staticmethod
+    def _mkdir(path):
+        """
+        Create a directory, but don't fail if it already exists
+        :param path:
+        """
+        try:
+            os.makedirs(path)
+        except OSError as exception:
+            if exception.errno != errno.EEXIST:
+                raise
 
     @property
     def sensor_data_filename(self):
