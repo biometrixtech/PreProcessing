544659d6d054fce59d2fc918af2edbc7842d7c58
Stephen Poole <stephen@melon.software>
2018-04-13 17:17:21 +0100

    Zip and upload lambda function bundles to S3

--- lambci.py
+++ lambci.py
@@ -27,6 +27,26 @@ def upload_cf_template(template, s3_bucket):
     s3_bucket.upload_file(local_filepath, s3_key)
 
 
+def upload_lambda_bundle(filename, s3_bucket):
+    filename = os.path.join(os.environ['SHALLOW_DIR'], filename)
+    print('Zipping bundle')
+    if filename[-3:] == '.py':
+        # Zipping one file
+        output_filename = filename.replace('.py', '.zip')
+        zipfile.ZipFile(output_filename + '.zip', mode='w').write(filename)
+    else:
+        # A whole bundle; install pip requirements first
+        os.chdir(filename)
+        subprocess.check_call('pip install -t . -r pip_requirements', shell=True)
+        os.chdir(os.environ['SHALLOW_DIR'])
+        shutil.make_archive(filename, 'zip', filename)
+        output_filename = filename + '.zip'
+
+    s3_key = 'lambdas/{}/{}/{}'.format(os.environ['PROJECT'], os.environ['LAMBCI_COMMIT'], os.path.basename(output_filename))
+    print('    Uploading {} to s3://{}/{}'.format(output_filename, s3_bucket.name, s3_key))
+    s3_bucket.upload_file(output_filename, s3_key)
+
+
 def main():
     os.environ['SHALLOW_DIR'] = os.environ['PWD']
     os.environ['PROJECT'] = os.environ['LAMBCI_REPO'].split('/')[-1].lower()
@@ -37,6 +57,9 @@ def main():
     for file in glob.glob(os.path.join(os.environ['SHALLOW_DIR'], 'cloudformation', '*.yaml')):
         file_name = os.path.basename(file)
         upload_cf_template(file_name, s3_bucket)
+
+    print("Deploying Lambda functions")
+    upload_lambda_bundle('apigateway', s3_bucket)
 
 
 if __name__ == '__main__':
