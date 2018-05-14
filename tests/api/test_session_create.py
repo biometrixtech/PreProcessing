963ab7a62ff0ef3c740af35df0077491c2b5d14f
Stephen Poole <stephen@melon.software>
2018-05-14 23:50:51 +0100

    More tests

--- tests/api/test_session_create.py
+++ tests/api/test_session_create.py
@@ -71,37 +71,3 @@ class TestSessionCreateOneSensor(BaseTest):
 
     def tearDown(self):
         sessions_table.delete_item(Key={'id': self.expected_id})
-
-
-# class TestFirmwareGetInvalidVersion(BaseTest):
-#     endpoint = 'firmware/accessory/fourtytwo'
-#     method = 'GET'
-#     expected_status = 404
-#
-#
-# class TestFirmwareGet10(BaseTest):
-#     endpoint = 'firmware/accessory/1.0'
-#     method = 'GET'
-#     expected_status = 200
-#
-#     def validate_response(self, body, headers, status):
-#         self.assertIn('firmware', body)
-#         self.assertIn('device_type', body['firmware'])
-#         self.assertEqual('accessory', body['firmware']['device_type'])
-#         self.assertIn('version', body['firmware'])
-#         self.assertEqual('1.0', body['firmware']['version'])
-#
-#
-# class TestFirmwareGetLatest(BaseTest):
-#     endpoint = 'firmware/accessory/latest'
-#     method = 'GET'
-#     expected_status = 200
-#
-#     def validate_response(self, body, headers, status):
-#         self.assertIn('firmware', body)
-#         self.assertIn('device_type', body['firmware'])
-#         self.assertEqual('accessory', body['firmware']['device_type'])
-#         self.assertIn('version', body['firmware'])
-#         self.assertNotEqual('latest', body['firmware']['version'])
-#         self.assertNotEqual('1.0', body['firmware']['version'])
-
