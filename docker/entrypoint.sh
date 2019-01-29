713c565c47acaeee41c7084fc3ad9f778bb0eac6
Stephen Poole <stephen@melon.software>
2019-01-29 16:34:25 +0000

    Use launch template to make compute cluster mount the EFS system per-instance, and avoid having to mount it in each container

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env sh
 
-# Mount the EFS directory
-echo "Mounting NFS directory"
-mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=10,retrans=2 efs.internal:/ /net/efs 2>&1
-
 # Execute the CMD from your Dockerfile, i.e. "npm start"
 echo "Executing batch_entrypoint.py"
-python ./batch_entrypoint.py "$1" "$2" "$3"
+python ./batch_entrypoint.py "$1"
