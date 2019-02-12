c77e883b755be27d4fd20a6fa5759d51d86c6453
Stephen Poole <stephen@melon.software>
2019-02-12 15:38:29 +0000

    Improve monitoring

--- lambdas/monitor/index.py
+++ lambdas/monitor/index.py
@@ -4,6 +4,7 @@ import datetime
 import itertools
 import os
 import re
+import time
 
 _cloudwatch_client = boto3.client('cloudwatch')
 _batch_client = boto3.client('batch')
@@ -17,21 +18,29 @@ def ignore_errors(f):
     """
     Invoke a function, catching and ignoring all errors
     """
+
     @wraps(f)
     def wrapper(*args, **kwargs):
         try:
             f(*args, **kwargs)
         except Exception as e:
             print(e)
+
     return wrapper
 
 
-def handler(_, __):
-    do_batch_desired_cpu()
-    do_batch_actual_cpu()
-    do_batch_job_counts()
-    do_efs_filesystem_size()
-    do_sfn_execution_counts()
+def handler(_, context):
+    global should_run_again
+    should_run_again = True
+
+    while context.get_remaining_time_in_millis() > 12 * 1000 and should_run_again:
+        print(context.get_remaining_time_in_millis())
+        do_batch_desired_cpu()
+        do_batch_actual_cpu()
+        do_batch_job_counts()
+        do_efs_filesystem_size()
+        do_sfn_execution_counts()
+        time.sleep((context.get_remaining_time_in_millis() % 10000 - 100) / 1000)
 
 
 @ignore_errors
@@ -43,53 +52,66 @@ def do_batch_desired_cpu():
 
 @ignore_errors
 def do_batch_actual_cpu():
-        container_instance_arns = [arn for arn in _ecs_client.list_container_instances(cluster=os.environ['BATCH_ECS_CLUSTER'])['containerInstanceArns']]
+    container_instance_arns = [arn for arn in _ecs_client.list_container_instances(cluster=os.environ['BATCH_ECS_CLUSTER_ARN'])['containerInstanceArns']]
 
-        if len(container_instance_arns):
-            ec2_instances = [instance['ec2InstanceId'] for instance in
-                             _ecs_client.describe_container_instances(cluster=os.environ['BATCH_ECS_CLUSTER'],
-                                                                      containerInstances=container_instance_arns)[
-                                 'containerInstances']]
-            data = _ec2_client.describe_instances(
-                InstanceIds=ec2_instances,
-                Filters=[{'Name': 'instance-state-name', 'Values': ['running']}]
-            )['Reservations']
+    if len(container_instance_arns):
+        container_instances = _ecs_client.describe_container_instances(
+            cluster=os.environ['BATCH_ECS_CLUSTER_ARN'],
+            containerInstances=container_instance_arns
+        )['containerInstances']
 
-            instance_type_cpus = {
-                r'c4\.8xlarge': 36,
-                r"m4\.10xlarge": 40,
-                r'..\.32xlarge': 128,
-                r'..\.16xlarge': 64,
-                r'..\.8xlarge': 32,
-                r'..\.4xlarge': 16,
-                r'..\.2xlarge': 8,
-                r'..\.xlarge': 4,
-                r'..\.large': 2,
-                r'..\.medium': 1,
-            }
+        data = _ec2_client.describe_instances(
+            InstanceIds=[instance['ec2InstanceId'] for instance in container_instances],
+            Filters=[{'Name': 'instance-state-name', 'Values': ['running']}]
+        )['Reservations']
 
-            def get_cpu(t):
-                for r, v in instance_type_cpus.items():
-                    if re.match(r, t):
-                        return v
+        instance_type_cpus = {
+            r'c4\.8xlarge': 36,
+            r"m4\.10xlarge": 40,
+            r'..\.32xlarge': 128,
+            r'..\.16xlarge': 64,
+            r'..\.8xlarge': 32,
+            r'..\.4xlarge': 16,
+            r'..\.2xlarge': 8,
+            r'..\.xlarge': 4,
+            r'..\.large': 2,
+            r'..\.medium': 1,
+        }
 
-            batch_actual_cpus = sum([get_cpu(instance['InstanceType']) for reservation in data for instance in reservation['Instances']])
-        else:
-            batch_actual_cpus = 0
-        _put_metric('BatchComputeEnvironmentActualCpus', batch_actual_cpus)
+        def get_cpu(t):
+            for r, v in instance_type_cpus.items():
+                if re.match(r, t):
+                    return v
+
+        batch_actual_cpus = sum(
+            [get_cpu(instance['InstanceType']) for reservation in data for instance in reservation['Instances']])
+    else:
+        batch_actual_cpus = 0
+    _put_metric('BatchComputeEnvironmentActualCpus', batch_actual_cpus)
 
 
 @ignore_errors
 def do_batch_job_counts():
-    for group, statuses in {'SCHEDULED': ['SUBMITTED', 'PENDING', 'RUNNABLE', 'STARTING'], 'RUNNING': ['RUNNING'],
-                            'SUCCEEDED': ['SUCCEEDED'], 'FAILED': ['FAILED']}.items():
-        group_jobs = []
+    status_groups = {
+        'SCHEDULED': ['SUBMITTED', 'PENDING', 'RUNNABLE', 'STARTING'],
+        'RUNNING': ['RUNNING'],
+        'SUCCEEDED': ['SUCCEEDED'],
+        'FAILED': ['FAILED'],
+    }
+    job_counts = {}
+    for status_group, statuses in status_groups.items():
         for status in statuses:
-            jobs = _batch_jobs_by_job(status)
-            _put_metric('BatchJobQueueCount', len(jobs), {'Status': status})
-            group_jobs += jobs
-        for job_name, jobs in itertools.groupby(sorted(group_jobs)):
-            _put_metric('BatchJobQueueCount', len(list(jobs)), {'Status': group, 'Job': job_name})
+            for job in _batch_jobs_by_status(status):
+                if 'stoppedAt' in job and time.time() * 1000 - job['stoppedAt'] > 60 * 5 * 1000:
+                    continue
+                job_name = job['jobName'].split('_')[-1]
+                job_counts.setdefault((job_name, status_group), 0)
+                job_counts.setdefault((None, status_group), 0)
+                job_counts[(job_name, status_group)] += 1
+                job_counts[(None, status_group)] += 1
+
+    for (job_name, status), count in job_counts.items():
+        _put_metric('BatchJobQueueCount', count, _dict_filter_values({'Status': status, 'Job': job_name}))
 
 
 @ignore_errors
@@ -105,24 +127,29 @@ def do_sfn_execution_counts():
     for status in ['RUNNING', 'SUCCEEDED', 'FAILED', 'TIMED_OUT', 'ABORTED']:
         _put_metric('StepFunctionsExecutions', executions.count(status), {'Status': status})
 
+        if status == 'RUNNING':
+            global should_run_again
+            should_run_again = executions.count(status) > 0
 
-def _batch_jobs_by_job(status, token=''):
-    jobs = _batch_client.list_jobs(jobQueue=os.environ['BATCH_JOB_QUEUE'], jobStatus=status, nextToken=token)
-    return [job['jobName'].split('-')[4] for job in jobs['jobSummaryList']] + (
-        _batch_jobs_by_job(status, jobs['nextToken']) if 'nextToken' in jobs and jobs['nextToken'] else [])
+
+def _batch_jobs_by_status(status, token=''):
+    res = _batch_client.list_jobs(jobQueue=os.environ['BATCH_JOB_QUEUE'], jobStatus=status, nextToken=token)
+    yield from res['jobSummaryList']
+    if 'nextToken' in res and res['nextToken']:
+        yield from _batch_jobs_by_status(status, res['nextToken'])
 
 
 def _list_sfn_executions(token=None):
-    if token is not None:
-        res = _sfn_client.list_executions(stateMachineArn=os.environ['STATE_MACHINE_ARN'], nextToken=token)
-    else:
-        res = _sfn_client.list_executions(stateMachineArn=os.environ['STATE_MACHINE_ARN'])
-    return res['executions'] + (
-        _list_sfn_executions(res['nextToken']) if 'nextToken' in res and res['nextToken'] else [])
+    res = _sfn_client.list_executions(
+        **_dict_filter_values({'stateMachineArn': os.environ['STATE_MACHINE_ARN'], 'nextToken': token}))
+    yield from res['executions']
+    if 'nextToken' in res and res['nextToken']:
+        yield from _list_sfn_executions(res['nextToken'])
 
 
 def _put_metric(metric_name, value, dimensions={}, unit='None'):
     dimensions = {**{'Environment': os.environ['ENVIRONMENT']}, **dimensions}
+    print(f'Putting {metric_name}={value} ({dimensions})')
     _cloudwatch_client.put_metric_data(
         Namespace='Preprocessing',
         MetricData=[{
@@ -133,3 +160,7 @@ def _put_metric(metric_name, value, dimensions={}, unit='None'):
             'Unit': unit
         }]
     )
+
+
+def _dict_filter_values(d):
+    return {k: v for k, v in d.items() if v}
