745c210c50a63335ec7fc996e130c354b3d6106d
DIpesh Gautam <dipesh@biometrixtech.com>
2019-05-19 11:16:48 -0400

    write to plans

--- app/jobs/advancedstats/__init__.py
+++ app/jobs/advancedstats/__init__.py
@@ -1,3 +1,4 @@
+from fathomapi.comms.service import Service
 from pymongo import ASCENDING
 import logging
 
@@ -13,6 +14,7 @@ class AdvancedstatsJob(Job):
         user_id = self.datastore.get_metadatum('user_id')
         event_date = self.datastore.get_metadatum('event_date')
         unit_blocks = get_unit_blocks(user_id, event_date)
+        self._write_session_to_plans(user_id, event_date)
 
         if len(unit_blocks) > 0:
             # Write out active blocks
@@ -32,6 +34,14 @@ class AdvancedstatsJob(Job):
             from .asymmetry_processor_job import AsymmetryProcessorJob
             AsymmetryProcessorJob(self.datastore, unit_blocks, cmj.motion_complexity_single_leg, cmj.motion_complexity_double_leg).run()
 
+    def _write_session_to_plans(self, user_id, event_date):
+        plans_service = Service('plans', '4_0')
+        body = {'user_id': user_id,
+                'event_date': event_date+'T10:00:00Z',
+                'sessions': [{'event_date': event_date+'T10:00:00Z'}]}
+        plans_service.call_apigateway_async(method='POST',
+                                            endpoint='/session/three_sensor_data',
+                                            body=body)
 
 def get_unit_blocks(user, date):
     """
@@ -48,3 +58,5 @@ def get_unit_blocks(user, date):
         {'unitBlocks': 1, '_id': 1, 'timeStart': 1, 'timeEnd': 1}).sort('unitBlocks.timeStart', direction=ASCENDING)
     )
     return unit_blocks
+
+
