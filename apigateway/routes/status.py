e5d830b4ca8f2192238e85a059538ea3d5dd4a25
DIpesh Gautam <dipesh@biometrixtech.com>
2019-06-07 16:53:47 -0400

    initial pass at sensor and files status

--- apigateway/routes/status.py
+++ apigateway/routes/status.py
@@ -11,26 +11,69 @@ from models.session import Session
 app = Blueprint('status', __name__)
 
 
-@app.route('/athlete', methods=['GET'])
+@app.route('/sensor', methods=['POST'])
 @require.authenticated.any
 @xray_recorder.capture('routes.status.get')
 def handle_get_upload_status(principal_id=None):
     user_id = principal_id
-    days = int(request.args.get('days', 30))
+    accessory_id = request.json.get('accessory_id', None)
+    minutes_offset = _get_offset(request.json.get('timezone', '-04:00'))
+    accessory = _get_accessory(accessory_id)
+
+    days = request.json.get('days', 30)
     current_time = datetime.datetime.now()
     sessions = list(Session.get_many(user_id=user_id,
                                      index='user_id-event_date'))
-    sessions = [s for s in sessions if s['event_date'] > format_datetime(current_time - datetime.timedelta(days=days))]
-    ret = []
+    sessions = [s for s in sessions if s['event_date'] > format_datetime(current_time - datetime.timedelta(days=days)) and
+                s.get('upload_end_date', None) is not None]
+    cleaned_sessions_list = []
     for session in sessions:
-        item = {}
-        item['event_date'] = session['event_date']
-        item['upload_status'] = 'UPLOAD_NOT_COMPLETE' if session['session_status'] in ['CREATE_COMPLETE', 'UPLOAD_IN_PROGRESS'] else 'UPLOAD_COMPLETE'
-        try:
-            item['duration'] = round((parse_datetime(session['end_date']) - parse_datetime(session['event_date'])).seconds / 60, 2)
-        except:
-            item['duration'] = None
-        ret.append(item)
-    return {"sessions": ret}, 200
+        cleaned_session = _get_cleaned_session(session, minutes_offset)
+        cleaned_sessions_list.append(cleaned_session)
+    return {"sessions": cleaned_sessions_list, "accessory": accessory}
 
- 
+
+def _get_offset(tz):
+    offset = tz.split(":")
+    hour_offset = int(offset[0])
+    minute_offset = int(offset[1])
+    if hour_offset < 0:
+        minute_offset = hour_offset * 60 - minute_offset
+    else:
+        minute_offset += hour_offset * 60
+    return minute_offset
+
+
+def _get_accessory(accessory_id):
+    if accessory_id is not None:
+        # get accessory from hardware api
+        accessory = {
+            "last_sync_date_time": None,
+            "battery_level": None,
+            "firmware_version": None,
+            "firmware_up_to_date": False
+        }
+    else:
+        # TODO handle this case properly
+        accessory = {
+            "last_sync_date_time": None,
+            "battery_level": None,
+            "firmware_version": None,
+            "firmware_up_to_date": False
+        }
+    return accessory
+
+
+def _get_cleaned_session(session, minutes_offset):
+    item = {}
+    event_date = parse_datetime(session['event_date']) - datetime.timedelta(minutes=minutes_offset)
+    end_date = parse_datetime(session['end_date']) - datetime.timedelta(minutes=minutes_offset)
+    upload_end_date = parse_datetime(session['upload_end_date']) - datetime.timedelta(minutes=minutes_offset)
+
+    item['event_date'] = format_datetime(event_date)
+    item['end_date'] = format_datetime(end_date)
+    item['upload_end_date'] = format_datetime(upload_end_date)
+    try:
+        item['duration'] = round((end_date - event_date).seconds / 60, 2)
+    except:
+        item['duration'] = None
