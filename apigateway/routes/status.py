19fc2dcfd9aefe794faefa8f38a5e6a2fea29cd5
DIpesh Gautam <dipesh@biometrixtech.com>
2019-06-10 14:34:31 -0400

    actually call hardware and add status

--- apigateway/routes/status.py
+++ apigateway/routes/status.py
@@ -1,6 +1,7 @@
 from flask import request, Blueprint
 import datetime
 
+from fathomapi.comms.service import Service
 from fathomapi.utils.decorators import require
 from fathomapi.utils.xray import xray_recorder
 from fathomapi.utils.formatters import format_datetime, parse_datetime
@@ -20,12 +21,11 @@ def handle_get_upload_status(principal_id=None):
     minutes_offset = _get_offset(request.json.get('timezone', '-04:00'))
     accessory = _get_accessory(accessory_id)
 
-    days = request.json.get('days', 30)
+    days = request.json.get('days', 14)
     current_time = datetime.datetime.now()
     sessions = list(Session.get_many(user_id=user_id,
                                      index='user_id-event_date'))
-    sessions = [s for s in sessions if s['event_date'] > format_datetime(current_time - datetime.timedelta(days=days)) and
-                s.get('upload_end_date', None) is not None]
+    sessions = [s for s in sessions if s['event_date'] > format_datetime(current_time - datetime.timedelta(days=days))]
     cleaned_sessions_list = []
     for session in sessions:
         cleaned_session = _get_cleaned_session(session, minutes_offset)
@@ -47,16 +47,22 @@ def _get_offset(tz):
 def _get_accessory(accessory_id):
     if accessory_id is not None:
         # get accessory from hardware api
-        accessory = {
-            "last_sync_date_time": None,
-            "battery_level": None,
-            "firmware_version": None,
-            "firmware_up_to_date": False
-        }
+        HARDWARE_API_VERSION = '2_0'
+        accessory_service = Service('hardware', HARDWARE_API_VERSION)
+        response = accessory_service.call_apigateway_sync(method='GET',
+                                                          endpoint=f'/accessory/{accessory_id}')
+        accessory = {}
+        accessory['last_sync_date'] = response['accessory'].get('last_sync_date', None)
+        accessory['battery_level'] = response['accessory'].get('battery_level', None)
+        accessory['firmware_version'] = response['accessory'].get('firmware_version', None)
+        if accessory['firmware_version'] == response['latest_firmware'].get('accessory_version', None):
+            accessory['firmware_up_to_date'] = True
+        else:
+            accessory['firmware_up_to_date'] = False
     else:
         # TODO handle this case properly
         accessory = {
-            "last_sync_date_time": None,
+            "last_sync_date": None,
             "battery_level": None,
             "firmware_version": None,
             "firmware_up_to_date": False
@@ -68,12 +74,23 @@ def _get_cleaned_session(session, minutes_offset):
     item = {}
     event_date = parse_datetime(session['event_date']) - datetime.timedelta(minutes=minutes_offset)
     end_date = parse_datetime(session['end_date']) - datetime.timedelta(minutes=minutes_offset)
-    upload_end_date = parse_datetime(session['upload_end_date']) - datetime.timedelta(minutes=minutes_offset)
 
     item['event_date'] = format_datetime(event_date)
     item['end_date'] = format_datetime(end_date)
-    item['upload_end_date'] = format_datetime(upload_end_date)
+    if session.get('upload_end_date', None) is not None:
+        upload_end_date = parse_datetime(session['upload_end_date']) - datetime.timedelta(minutes=minutes_offset)
+        item['upload_end_date'] = format_datetime(upload_end_date)
+    else:
+        item['upload_end_date'] = None
+    session_status = session('session_status', None)
+    if session_status in ['CREATE_COMPLETE', 'UPLOAD_IN_PROGRESS', 'UPLOAD_COMPLETE', 'PROCESSING_IN_PROGRESS']:
+        item['status'] = 0
+    elif session_status in ['PROCESSING_COMPLETE']:
+        item['status'] = 1
+    else:
+        item['status'] = 2
     try:
         item['duration'] = round((end_date - event_date).seconds / 60, 2)
     except:
         item['duration'] = None
+    return item
