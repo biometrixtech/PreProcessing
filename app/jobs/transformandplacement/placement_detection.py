f15ab707bd5f0b2d19a5f0f6a567c28fdac6f08a
Paul LaForge <paul@biometrixtech.com>
2019-07-05 16:31:55 -0500

    moved placement code into core

--- app/jobs/transformandplacement/placement_detection.py
+++ app/jobs/transformandplacement/placement_detection.py
@@ -1,12 +1,19 @@
 from __future__ import print_function
-
+import logging
 import numpy as np
+import pandas as pd
 from scipy.stats import skew
 
 import utils.quaternion_conversions as qc
 import utils.quaternion_operations as qo
+from jobs.transformandplacement import PlacementDetectionException
+from utils import quaternion_conversions as qc
+from utils.detect_peaks import detect_peaks
 from .exceptions import PlacementDetectionException
 from .column_vector import MatchProbability
+
+
+_logger = logging.getLogger(__name__)
 
 
 def detect_placement(data):
@@ -225,132 +232,129 @@ def predict_placement(data, condition_list):
             return placement, {'left': result[0].upper(), 'right': result[1].upper()}
 
 
-def detect_placement_lateral_hip(data):
-    correct_list = []
-    error_list = []
-    help_list = []
-    zero_list = []
-    file_starts = 800
-    file_ends = 1400
-    index_num = 0
-    
-    hip_accel_data = data["acc_1_y"][file_starts:file_ends]
-    sensor_0_accel_data = data["acc_lf_z"][file_starts:file_ends]
-    sensor_2_accel_data = data["acc_rf_z"][file_starts:file_ends]
-    lf_euls = qc.quat_to_euler(
-        data["quat_lf_w"][file_starts:file_ends],
-        data["quat_lf_x"][file_starts:file_ends],
-        data["quat_lf_y"][file_starts:file_ends],
-        data["quat_lf_z"][file_starts:file_ends])
-    sensor_0_euler_y_degrees = pd.Series(lf_euls[:, 1] * 180/ np.pi)
-    rf_euls = qc.quat_to_euler(
-        data["quat_rf_w"][file_starts:file_ends],
-        data["quat_rf_x"][file_starts:file_ends],
-        data["quat_rf_y"][file_starts:file_ends],
-        data["quat_rf_z"][file_starts:file_ends])
-    sensor_2_euler_y_degrees = pd.Series(rf_euls[:, 1] * 180 / np.pi)
+def get_placement_lateral_hip(data, window_start, window_end, session_id=''):
 
-    trough_mpd = 1
-    trough_mph = 20
-    trough_edge = 'rising'
-    trough_thresh = 0
-    sensor_0_peaks = detect_peaks(sensor_0_euler_y_degrees,mph=trough_mph, mpd=trough_mpd,threshold=trough_thresh,edge=trough_edge,kpsh=False,valley=False)
-    sensor_2_peaks = detect_peaks(sensor_2_euler_y_degrees,mph=trough_mph, mpd=trough_mpd, threshold=trough_thresh, edge=trough_edge, kpsh=False, valley=False)
-    troughs = []
-    for lf in sensor_0_peaks:
-        troughs.append(("L", lf))  # we'll be using the same point in time with a diff data series
+    try:
+        hip_accel_data = data["acc_1_y"][window_start:window_end]
+        sensor_0_accel_data = data["acc_0_z"][window_start:window_end]
+        sensor_2_accel_data = data["acc_2_z"][window_start:window_end]
+        lf_euls = qc.quat_to_euler(
+            data["quat_0_w"][window_start:window_end],
+            data["quat_0_x"][window_start:window_end],
+            data["quat_0_y"][window_start:window_end],
+            data["quat_0_z"][window_start:window_end])
+        sensor_0_euler_y_degrees = pd.Series(lf_euls[:, 1] * 180 / np.pi)
+        rf_euls = qc.quat_to_euler(
+            data["quat_2_w"][window_start:window_end],
+            data["quat_2_x"][window_start:window_end],
+            data["quat_2_y"][window_start:window_end],
+            data["quat_2_z"][window_start:window_end])
+        sensor_2_euler_y_degrees = pd.Series(rf_euls[:, 1] * 180 / np.pi)
 
-    for rf in sensor_2_peaks:
-        troughs.append(("R", rf))  # we'll be using the same point in time with a diff data series
+        trough_mpd = 1
+        trough_mph = 20
+        trough_edge = 'rising'
+        trough_thresh = 0
+        sensor_0_peaks = detect_peaks(sensor_0_euler_y_degrees, mph=trough_mph, mpd=trough_mpd, threshold=trough_thresh,
+                                      edge=trough_edge, kpsh=False, valley=False)
+        sensor_2_peaks = detect_peaks(sensor_2_euler_y_degrees, mph=trough_mph, mpd=trough_mpd, threshold=trough_thresh,
+                                      edge=trough_edge, kpsh=False, valley=False)
+        troughs = []
+        for s_0 in sensor_0_peaks:
+            troughs.append(("0", s_0))  # we'll be using the same point in time with a diff data series
+        for s_2 in sensor_2_peaks:
+            troughs.append(("2", s_2))  # we'll be using the same point in time with a diff data series
+        troughs = sorted(troughs, key=lambda x: x[1])
+        crossing_zero = []
+        crossing_zero = sorted(crossing_zero, key=lambda x: x[1])
+        zero_cross = crossing_zero
 
-    troughs = sorted(troughs, key=lambda x: x[1])
+        for p in range(0, len(troughs)):
+            if troughs[p][0] == "0":
+                if p == len(troughs) - 1:
+                    sensor_0_accel_short_data = sensor_0_accel_data.values[troughs[p][1]:]
 
-    crossing_zero = []
+                else:
+                    sensor_0_accel_short_data = sensor_0_accel_data.values[troughs[p][1]:troughs[p + 1][1]]
 
-    crossing_zero = sorted(crossing_zero, key=lambda x: x[1])
-    zero_cross = crossing_zero
-
-    for p in range(0, len(troughs)):
-        if troughs[p][0] == "L":
-            if p == len(troughs) - 1:
-                sensor_0_accel_short_data = sensor_0_accel_data.values[troughs[p][1]:]
+                zero_crossings = np.where(np.diff(np.sign(sensor_0_accel_short_data)))[0]
+                if len(zero_crossings) > 0:
+                    crossing_zero.append(("0", zero_crossings[0] + 1 + troughs[p][1]))
 
             else:
-                sensor_0_accel_short_data = sensor_0_accel_data.values[troughs[p][1]:troughs[p+1][1]]
+                if p == len(troughs) - 1:
 
-            zero_crossings = np.where(np.diff(np.sign(sensor_0_accel_short_data)))[0]
-            if len(zero_crossings) > 0:
-                crossing_zero.append(("L", zero_crossings[0]+1 + troughs[p][1]))
+                    sensor_2_accel_short_data = sensor_2_accel_data.values[troughs[p][1]:]
+                else:
 
-        else:
-            if p == len(troughs) - 1:
+                    sensor_2_accel_short_data = sensor_2_accel_data.values[troughs[p][1]:troughs[p + 1][1]]
 
-                sensor_2_accel_short_data = sensor_2_accel_data.values[troughs[p][1]:]
+                zero_crossings = np.where(np.diff(np.sign(sensor_2_accel_short_data)))[0]
+                if len(zero_crossings) > 0:
+                    crossing_zero.append(("2", zero_crossings[0] + 1 + troughs[p][1]))
+
+        sensor_dict = {}
+        sensor_dict["0T"] = 0
+        sensor_dict["0P"] = 0
+        sensor_dict["2T"] = 0
+        sensor_dict["2P"] = 0
+        for c in range(0, len(crossing_zero) - 1):
+
+            if c < len(crossing_zero) - 1:
+                window_data = hip_accel_data[crossing_zero[c][1]:crossing_zero[c + 1][1]]
             else:
+                window_data = hip_accel_data[crossing_zero[c][1]:]
 
-                sensor_2_accel_short_data = sensor_2_accel_data.values[troughs[p][1]:troughs[p + 1][1]]
+            window_results = []
+            mpd = 1
+            mph = 5.0
+            thresh = 0
+            edge = None  # has no impact on detection
 
-            zero_crossings = np.where(np.diff(np.sign(sensor_2_accel_short_data)))[0]
-            if len(zero_crossings) > 0:
-                crossing_zero.append(("R", zero_crossings[0]+1 + troughs[p][1]))
+            if crossing_zero[c][0] == "0":
+                window_troughs_0 = detect_peaks(window_data.values, mph=mph, mpd=mpd, threshold=thresh, edge=edge,
+                                                kpsh=False, valley=True)
+                for w_0 in window_troughs_0:
+                    window_results.append(("0", "T", w_0))
+                window_peaks_0 = detect_peaks(window_data.values, mph=mph, mpd=mpd, threshold=thresh, edge=edge, kpsh=False,
+                                              valley=False)
+                for w_0 in window_peaks_0:
+                    window_results.append(("0", "P", w_0))
+            else:
+                window_troughs_2 = detect_peaks(window_data.values, mph=mph, mpd=mpd, threshold=thresh, edge=edge,
+                                                kpsh=False, valley=True)
+                for w_2 in window_troughs_2:
+                    window_results.append(("2", "T", w_2))
+                window_peaks_2 = detect_peaks(window_data.values, mph=mph, mpd=mpd, threshold=thresh, edge=edge, kpsh=False,
+                                              valley=False)
+                for w_2 in window_peaks_2:
+                    window_results.append(("2", "P", w_2))
 
-    sensor_dict = {}
-    sensor_dict["LT"] = 0
-    sensor_dict["LP"] = 0
-    sensor_dict["RT"] = 0
-    sensor_dict["RP"] = 0
+            window_results = sorted(window_results, key=lambda x: x[2])
 
-    for c in range(0, len(crossing_zero) - 1):
+            if len(window_results) > 0:
+                if window_results[0][0] == "0" and window_results[0][1] == "T":
+                    sensor_dict["0T"] += 1
+                elif window_results[0][0] == "0" and window_results[0][1] == "P":
+                    sensor_dict["0P"] += 1
+                elif window_results[0][0] == "2" and window_results[0][1] == "T":
+                    sensor_dict["2T"] += 1
+                elif window_results[0][0] == "2" and window_results[0][1] == "P":
+                    sensor_dict["2P"] += 1
+        side_0 = sensor_dict["0T"] + sensor_dict["2P"]
+        side_2 = sensor_dict["0P"] + sensor_dict["2T"]
 
-        if c < len(crossing_zero) - 1:
-            window_data = hip_accel_data[crossing_zero[c][1]:crossing_zero[c + 1][1]]
-        else:
-            window_data = hip_accel_data[crossing_zero[c][1]:]
+        if abs(side_0 - side_2) == 1:# or (side_2 + side_0) <= 2:
+            _logger.info("Placement detection weak for session_id="+session_id)
 
-        window_results = []
-        mpd = 1
-        mph = 5.0
-        thresh = 0
-        edge = None  # has no impact on detection
+        if side_0 > side_2:
+            return [0, 1, 2]
+        elif side_2 > side_0:
+            return [2, 1, 0]
 
-        if crossing_zero[c][0] == "L":
-            left_window_troughs = detect_peaks(window_data.values, mph=mph, mpd=mpd, threshold=thresh, edge=edge, kpsh=False, valley=True)
-            for lf in left_window_troughs:
-                window_results.append(("L", "T", lf))
-            left_window_peaks = detect_peaks(window_data.values, mph=mph, mpd=mpd, threshold=thresh, edge=edge, kpsh=False, valley=False)
-            for lf in left_window_peaks:
-                window_results.append(("L", "P", lf))
-        else:
-            right_window_troughs = detect_peaks(window_data.values, mph=mph,mpd=mpd, threshold=thresh, edge=edge, kpsh=False, valley=True)
-            for rf in right_window_troughs:
-                window_results.append(("R", "T", rf))
-            right_window_peaks = detect_peaks(window_data.values, mph=mph,mpd=mpd, threshold=thresh, edge=edge, kpsh=False, valley=False)
-            for rf in right_window_peaks:
-                window_results.append(("R", "P", rf))
+    except Exception as e:
+        print(e)
+        _logger.error(e)
 
-        window_results = sorted(window_results, key=lambda x: x[2])
-
-        if len(window_results) > 0:
-            if window_results[0][0] == "L" and window_results[0][1] == "T":
-                sensor_dict["LT"] += 1
-            elif window_results[0][0] == "L" and window_results[0][1] == "P":
-                sensor_dict["LP"] += 1
-            elif window_results[0][0] == "R" and window_results[0][1] == "T":
-                sensor_dict["RT"] += 1
-            elif window_results[0][0] == "R" and window_results[0][1] == "P":
-                sensor_dict["RP"] += 1
-
-    left_side = sensor_dict["LT"] + sensor_dict["RP"]
-    right_side = sensor_dict["LP"] + sensor_dict["RT"]
-
-    if right_side > left_side:
-        error_list.append(file_num)
-    elif left_side == 0:
-        zero_list.append(file_num)
-    else:
-        correct_list.append(file_num)
-
-    if left_side - right_side == 1:
-        help_list.append(file_num)
-    index_num += 1
-    i=0
+    #raise PlacementDetectionException('Could not detect placement using any of the movements')
+    j=0
