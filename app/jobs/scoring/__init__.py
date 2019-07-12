fd4d9201d4e37c2dbf5e37d1d290bf2bf4e75f81
DIpesh Gautam <dipesh@biometrixtech.com>
2019-07-12 10:31:18 -0400

    write processed file to s3

--- app/jobs/scoring/__init__.py
+++ app/jobs/scoring/__init__.py
@@ -1,6 +1,8 @@
 from __future__ import print_function
 from aws_xray_sdk.core import xray_recorder
 import logging
+import boto3
+import os
 # import numpy as np
 
 from ..job import Job
@@ -8,66 +10,24 @@ from ..job import Job
 # from .scoring import score
 
 _logger = logging.getLogger()
+_s3_client = boto3.client('s3')
 
 _output_columns = [
     'obs_index',
+    'static_lf',
+    'static_hip',
+    'static_rf',
     'time_stamp',
     'epoch_time',
     'ms_elapsed',
-    # 'session_duration',
     'active',
-    # 'loading_lf',
-    # 'loading_rf',
     'phase_lf',
     'phase_rf',
-    # 'impact_phase_lf',
-    # 'impact_phase_rf',
     'grf',
     'grf_lf',
     'grf_rf',
-    # 'const_grf',
-    # 'dest_grf',
-    # 'destr_multiplier',
-    # 'session_grf_elapsed',
-    # 'symmetry',
-    # 'symmetry_l',
-    # 'symmetry_r',
-    # 'hip_symmetry',
-    # 'hip_symmetry_l',
-    # 'hip_symmetry_r',
-    # 'ankle_symmetry',
-    # 'ankle_symmetry_l',
-    # 'ankle_symmetry_r',
-    # 'consistency',
-    # 'hip_consistency',
-    # 'ankle_consistency',
-    # 'consistency_lf',
-    # 'consistency_rf',
-    # 'control',
-    # 'hip_control',
-    # 'ankle_control',
-    # 'control_lf',
-    # 'control_rf',
-    # 'contra_hip_drop_lf',
-    # 'contra_hip_drop_rf',
-    # 'ankle_rot_lf',
-    # 'ankle_rot_rf',
-    # 'foot_position_lf',
-    # 'foot_position_rf',
-    # 'land_pattern_lf',
-    # 'land_pattern_rf',
-    # 'land_time',
-    # 'rate_force_absorption_lf',
-    # 'rate_force_absorption_rf',
-    # 'rate_force_production_lf',
-    # 'rate_force_production_rf',
     'total_accel',
     'stance',
-    # 'plane',
-    # 'rot',
-    # 'lat',
-    # 'vert',
-    # 'horz',
     'adduc_motion_covered_abs_lf', 'adduc_motion_covered_pos_lf', 'adduc_motion_covered_neg_lf',
     'adduc_range_of_motion_lf',
     'flex_motion_covered_abs_lf', 'flex_motion_covered_pos_lf', 'flex_motion_covered_neg_lf',
@@ -117,16 +77,13 @@ class ScoringJob(Job):
         # Round the data to 6th decimal point
         data = data.round(6)
 
-        # Add nans for future variables
-        # data['symmetry_l'] = np.nan
-        # data['symmetry_r'] = np.nan
-        # data['hip_symmetry_l'] = np.nan
-        # data['hip_symmetry_r'] = np.nan
-        # data['ankle_symmetry_l'] = np.nan
-        # data['ankle_symmetry_r'] = np.nan
-
         # TODO replace computing boundaries for twoMin by active blocks
 
         # Output data
         self.datastore.put_data('scoring', data, columns=_output_columns)
         _logger.info("DONE WRITING OUTPUT FILE")
+
+        # Upload processed file to s3
+        s3_key = self.datastore.session_id + '_processed'
+        _logger.info('Uploading processed file to "s3://biometrix-decode/{}",'.format(s3_key))
+        _s3_client.upload_file(os.path.join(self.datastore.working_directory, 'scoring'), 'biometrix-decode', s3_key)
