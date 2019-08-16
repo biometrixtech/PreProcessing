852e2a482d570f688636a31dce5863afa2bdc211
DIpesh Gautam <dipesh@biometrixtech.com>
2019-08-16 16:07:21 -0400

    cleanup

--- app/jobs/driftcorrection/change_of_direction.py
+++ app/jobs/driftcorrection/change_of_direction.py
@@ -30,9 +30,9 @@ def flag_change_of_direction(acc_hip_z, euler_hip_z_hc):
         for i in range(look_backs, len(headings_at_peak) - 1):
             heading_diff[i] = np.abs(headings_at_peak[i] - headings_at_peak[i-look_backs])
 
-        # find indices where chage is > 40 degrees
+        # find indices where change is > 40 degrees
         heading_change_indices = np.where(heading_diff > 40)[0].astype(list)
-        change_of_direction = np.zeros(len(acc_hip_z))
+
         # mark "steps_to_remove" steps before and after identified heading change point
         for i in heading_change_indices:
             change_of_direction[peaks[i - steps_to_remove]:peaks[min([i + steps_to_remove, len(peaks) - 1])]] = 1
@@ -41,6 +41,6 @@ def flag_change_of_direction(acc_hip_z, euler_hip_z_hc):
         ranges, lengths = get_ranges(change_of_direction, 0, True)
         for r, l in zip(ranges, lengths):
             if l < 200:
-                change_of_direction[r[0]: r[1]] = 1
+                change_of_direction[r[0]:r[1]] = 1
 
     return change_of_direction
