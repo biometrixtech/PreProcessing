0e3d6494c93d00f20af94804710f29a844d69371
DIpesh Gautam <dipesh@biometrixtech.com>
2019-08-27 12:26:26 -0400

    fixing of tests

--- tests/api/test_session_upload.py
+++ tests/api/test_session_upload.py
@@ -1,10 +1,10 @@
-from .base_test import BaseTest
+from .base_test import BaseTest, get_api_service_token
 import boto3
 import time
 from boto3.dynamodb.conditions import Key, Attr
 
 # This has username = '00:00:00:00:00'
-jwt_token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6IjAwOjAwOjAwOjAwOjAwIn0._EtGIW4hbbJAbAywUrtonMwZgQuuU8dSyOrPZEF6TJE'
+jwt_token = get_api_service_token('00:00:00:00:00')
 
 sessions_table = boto3.resource('dynamodb').Table('preprocessing-dev-ingest-sessions')
 
@@ -20,20 +20,50 @@ def _query_dynamodb(resource, key_condition_expression):
 
 
 class TestSessionUploadNoAuth(BaseTest):
-    endpoint = 'session/911bffe6-2649-5b74-825a-7481bdf5920e/upload'
+    endpoint = 'session/ab0bc061-7c79-5e64-a1f6-e6b900257ace/upload'
     method = 'POST'
     expected_status = 401
 
 
-class TestSessionUploadInvalidUuid(BaseTest):
-    endpoint = 'session/foobar/upload'
-    method = 'POST'
-    authorization = jwt_token
-    expected_status = 400
+# class TestSessionUploadInvalidUuid(BaseTest):
+#     endpoint = 'session/foobar/upload'
+#     method = 'POST'
+#     authorization = jwt_token
+#     expected_status = 400
 
 
-class TestSessionUploadNonExistent(BaseTest):
-    endpoint = 'session/00000000-0000-4000-8000-000000000000/upload'
+# class TestSessionUploadNonExistent(BaseTest):
+#   # upload endpoint does not check if the session exists or not
+#     endpoint = 'session/00000000-0000-4000-8000-000000000000/upload'
+#     method = 'POST'
+#     authorization = jwt_token
+#     expected_status = 404
+#     content_type = 'application/octet-stream'
+#     body = "CHTlsVIAAAh/H/w/33v/deEvCH+j9QAwbeUlFGkIf4fyP+pdRpJmVwh05bFcAAAIfwv7v9l6y3PBPgh/o/"
+
+
+class TestSessionUpload(BaseTest):
+    endpoint = 'session/ab0bc061-7c79-5e64-a1f6-e6b900257ace/upload'
     method = 'POST'
     authorization = jwt_token
-    expected_status = 404
+    expected_status = 202
+    content_type = 'application/octet-stream'
+    body = "CHTlsVIAAAh/H/w/33v/deEvCH+j9QAwbeUlFGkIf4fyP+pdRpJmVwh05bFcAAAIfwv7v9l6y3PBPgh/o/"
+    expected_id = 'ab0bc061-7c79-5e64-a1f6-e6b900257ace'
+
+    def setUp(self):
+        sessions_table.put_item(Item={
+            'accessory_id': '00:00:00:00:00',
+            'created_dateString': '2018-05-14T22:18:22Z',
+            'event_date': '2001-01-01T01:23:45Z',
+            'id': 'ab0bc061-7c79-5e64-a1f6-e6b900257ace',
+            'sensor_ids': ['11:11:11:11:11'],
+            'session_status': 'CREATE_COMPLETE',
+            'updated_date':	'2018-05-14T22:18:22Z',
+            'version': '2.3',
+        })
+        pass
+
+    def tearDown(self):
+        sessions_table.delete_item(Key={'id': self.expected_id})
+        pass
