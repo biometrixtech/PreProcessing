12fc759bfd6334f8452ce8b22499460150c67709
DIpesh Gautam <dipesh@biometrixtech.com>
2019-08-27 10:50:11 -0400

    slightly more restrictive still detection

--- app/jobs/transformandplacement/get_march_and_still.py
+++ app/jobs/transformandplacement/get_march_and_still.py
@@ -5,14 +5,20 @@ from utils import get_ranges
 from utils.quaternion_conversions import quat_to_euler
 
 
+# constants
+march_detection_start = 775
+march_detection_end = 2000
+samples_before_march = 75
+
+
 def detect_march_and_still(data):
     start_still_0, end_still_0, start_march_0, end_march_0 = _detect_march_and_still_ankle(data, 0)
     start_still_2, end_still_2, start_march_2, end_march_2 = _detect_march_and_still_ankle(data, 2)
     start_march_hip = max([start_march_0, start_march_2])
     end_march_hip = min([end_march_0, end_march_2])
-    start_still_hip, end_still_hip = _detect_still(data[start_march_hip - 75:start_march_hip], sensor=1)
-    start_still_hip += start_march_hip - 75
-    end_still_hip += start_march_hip - 75
+    start_still_hip, end_still_hip = _detect_still(data[start_march_hip - samples_before_march:start_march_hip], sensor=1)
+    start_still_hip += start_march_hip - samples_before_march
+    end_still_hip += start_march_hip - samples_before_march
 
     return (start_march_0, end_march_0, start_still_0, end_still_0,
             start_march_hip, end_march_hip, start_still_hip, end_still_hip,
@@ -21,13 +27,13 @@ def detect_march_and_still(data):
 
 def _detect_march_and_still_ankle(data, sensor):
     # get march
-    start_march, end_march = _detect_march(data[f'static_{sensor}'][int(8 * 97.52):int(20*97.52)])
+    start_march, end_march = _detect_march(data[f'static_{sensor}'][march_detection_start:march_detection_end])
     if end_march != 0:
-        start_march += int(8 * 97.52)
-        end_march += int(8 * 97.52)
-        start_still, end_still = _detect_still(data[start_march - 75:start_march], sensor=0)
-        start_still += start_march - 75
-        end_still += start_march - 75
+        start_march += march_detection_start
+        end_march += march_detection_start
+        start_still, end_still = _detect_still(data[start_march - samples_before_march:start_march], sensor=sensor)
+        start_still += start_march - samples_before_march
+        end_still += start_march - samples_before_march
         return start_still, end_still, start_march, end_march
     else:
         raise ValueError(f'Could not detect march for sensor{sensor}')
@@ -38,7 +44,7 @@ def _detect_march(static):
     ranges, lengths = get_ranges(static, 1, True)
     for r, length in zip(ranges, lengths):
         if length >= 400:
-            return r[0], min(r[1], r[0] + int(6 * 97.52))
+            return r[0], min(r[1], r[0] + int(600))
     return 0, 0
 
 
@@ -61,6 +67,11 @@ def _detect_still(data, sensor=0):
     )
     euler_x = euler[:, 0] * 180 / np.pi
     euler_y = euler[:, 1] * 180 / np.pi
+
+    x_diff = max(euler_x) - min(euler_x)
+    y_diff = max(euler_y) - min(euler_y)
+    max_diff_all = max(x_diff, y_diff)
+
     sd_x = pd.Series(euler_x).rolling(25).std(center=True)
     sd_y = pd.Series(euler_y).rolling(25).std(center=True)
     sd_tilt = sd_x + sd_y
@@ -76,7 +87,7 @@ def _detect_still(data, sensor=0):
 
     still_present = data[f'static_{sensor}'][start:end].values == 0
     still = sum(still_present)
-    if still > 0 or max_diff < 1:
+    if (still > 1 or max_diff < .75) and max_diff_all < 20:
         return start, end
     else:
         raise ValueError(f'could not detect still for sensor{sensor}')
